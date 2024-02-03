6e9e20591b5c925a0cfa09b1ae80495350ee9657
calvinguyen <77700030+calvinguyen@users.noreply.github.com>
2024-02-02 19:05:39 -0800

    Add get, put, and delete user routes

--- server/src/routes/userRoute.ts
+++ server/src/routes/userRoute.ts
@@ -3,17 +3,63 @@ import User from '../models/userModel';
 
 const router = express.Router();
 
+// get all users
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    const allUsers = await User.find({});
+    res.status(200).json(allUsers);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// get a user by id
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// create new user
 router.post('/signup', async (req: Request, res: Response) => {
   const { firstName, lastName, description } = req.body;
+  try {
+    const newUser = new User({
+      firstName,
+      lastName,
+      description,
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  const newUser = new User({
-    firstName,
-    lastName,
-    description,
-  });
+// update a user
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    const userUpdated = await User.findById(req.params.id);
+    res.status(201).json(userUpdated);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-  const savedUser = await newUser.save();
-  res.status(201).json(savedUser);
+// delete a user by id
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json('Account has been deleted');
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 export default router;
