59663ac3ce6bcd7a96abe7757ae5e4fadef388c3
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-03 20:27:09 -0600

    Integrated new user embedding to be stored in pinecone db (very spaghetti)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,51 @@
 import app from "./app";
 import { connectToDatabase } from "./db/mongodb-connection";
-import { pineconeTest } from "./db/pineconedb-connection";
+import { pineconeTest, getPineconeClient } from "./db/pineconedb-connection";
+import { chatModelTest } from "./lib/embeddings";
+import Users from './models/userModel';
+import { OpenAIEmbeddings } from "@langchain/openai";
 
 const PORT = process.env.PORT || 5000;
 
 // Initalizing MongoDB connection
-connectToDatabase().then(() => {
+connectToDatabase().then(async () => {
     app.listen(PORT, () => console.log(`Server listening on port ${PORT} & connected to database.`));
     })
     .catch(error => console.log(error));
 
 // Initalizing PineconeDB connection
-pineconeTest().then(() => {
-    console.log("Pinecone connection successful.");
-}).catch(error => console.log(error));
+// pineconeTest().then(() => {
+//     console.log("Pinecone connection successful.");
+// }).catch(error => console.log(error));
+
+// Initalizing Chat model connection
+// chatModelTest().then(() => {
+//     console.log("Chat model connection successful.");
+// }).catch(error => console.log(error));
+
+Users.watch().on('change', async (data) => {
+    const embeddingsModel = new OpenAIEmbeddings();
+    const pineconeClient = await getPineconeClient();
+    const pineconeIndex = await pineconeClient.index("h2jc");
+
+    // If a new document is inserted into the collection, replicate its vector in Pinecone
+    if (data.operationType === 'insert') {
+        let document = data.fullDocument;
+        let docId = document._id;
+
+        if (!Array.isArray(document)) {
+            document = [document];
+        }
+
+        document = document.map(t => String(t));
+
+        const embeddings = await embeddingsModel.embedDocuments(document);
+
+        const records = embeddings.map((embedding) => ({
+            id: `${docId}`,
+            values: embedding,
+        }));
+        
+        await pineconeIndex.upsert(records);
+    }
+  });
--- server/src/services/mongodb-pinecone.ts
+++ server/src/services/mongodb-pinecone.ts
@@ -1,44 +1,32 @@
-//! Mongoose Library
 import Users from '../models/userModel';
-// import pinecone
-// import langchain
 
 Users.watch().on('change', (data) => {
   // If a new document is inserted into the collection, replicate its vector in Pinecone
   if (data.operationType === 'insert') {
     const document = data.fullDocument;
 
-    // Convert the document's name into an embedding
-    const vector = model.encode(document.name);
-
-    // Insert into Pinecone
-    pinecone.insert({
-      indexName: 'myindex',
-      data: vector,
-      ids: [String(document._id)],
-    });
   }
 
   // If a document is updated in the collection, update its vector in Pinecone
-  else if (data.operationType === 'update') {
-    const documentId = data.documentKey._id;
-    const updatedFields = data.updateDescription.updatedFields;
+  // else if (data.operationType === 'update') {
+  //   const documentId = data.documentKey._id;
+  //   const updatedFields = data.updateDescription.updatedFields;
 
-    // If the change is in the name field, generate the embedding and insert
-    if (updatedFields?.name) {
-      const vector = model.encode(updatedFields.name);
-      pinecone.upsert({
-        indexName: 'myindex',
-        data: vector,
-        ids: [String(documentId)],
-      });
-    }
-  }
+  //   // If the change is in the name field, generate the embedding and insert
+  //   if (updatedFields?.name) {
+  //     const vector = model.encode(updatedFields.name);
+  //     pinecone.upsert({
+  //       indexName: 'myindex',
+  //       data: vector,
+  //       ids: [String(documentId)],
+  //     });
+  //   }
+  // }
 
   // If a document is deleted from the collection, remove its vector from Pinecone
-  else if (data.operationType === 'delete') {
-    pinecone.delete({
-      ids: [String(data.documentKey._id)],
-    });
-  }
+  // else if (data.operationType === 'delete') {
+  //   pinecone.delete({
+  //     ids: [String(data.documentKey._id)],
+  //   });
+  // }
 });
