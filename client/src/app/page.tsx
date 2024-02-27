a71cb27db134e801acc624e74487b3f04ae8ec59
Henry Le <henrycle16@gmail.com>
2024-02-26 21:06:33 -0600

    inital landing page as well as adding MUI package. Going to add scrolling feature later

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import TextInputForm from "../components/TextInputForm";
+import HeroSection from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <>
-    </>
+   <main>
+   <HeroSection/>
+   </main>
   );
 }
