9884e1693abd4e33715e5b2d77bb888e51fd75df
Henry Le <henrycle16@gmail.com>
2024-03-11 20:39:41 -0500

    Creating nested page for Brand and Creator.

---
+++ client/src/app/signup/brand/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function BrandQuestions() {
+  return (
+    <main>FILL OUT THE FORM FOR BRAND</main>
+  )
+}
---
+++ client/src/app/signup/creator/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreatorQuestions() {
+  return (
+    <main>FILL OUT THE FORM FOR CREATOR</main>
+  )
+}
