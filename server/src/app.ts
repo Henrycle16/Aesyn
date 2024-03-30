38e72d591641b85708aea1b7b33f9d4cb2832b63
Henry Nguyen <nguyenhenry682@gmail.com>
2024-03-30 17:03:21 -0500

    imported cors and instagram route

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,11 +4,19 @@ import userRoute from './routes/userRoute';
 import queriesRoute from './routes/queriesRoute';
 import authRoute from './routes/authRoute';
 import creatorRoute from './routes/creatorRoute';
+import instagramRoute from './routes/instagramRoute';
+import cors from "cors";
 
 config();
 
 // Initalizing express server
 const app = express();
+
+app.use(cors(
+    {
+        origin: '*'
+    }
+));
 
 app.use(express.json());
 
@@ -17,6 +25,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/creator', creatorRoute);
 app.use('/api/query', queriesRoute);
+app.use('/api/instagram', instagramRoute);
 
 console.log("App is running");
 
