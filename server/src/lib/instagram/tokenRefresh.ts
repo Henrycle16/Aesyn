2693635575c376ca58695a3278c149f258140074
Henry Nguyen <nguyenhenry682@gmail.com>
2024-04-09 17:53:59 -0500

    Added feature to search through database for tokens that are X days old and refreshes them

--- server/src/lib/instagram/tokenRefresh.ts
+++ server/src/lib/instagram/tokenRefresh.ts
@@ -1,24 +1,27 @@
-import instagram_data from '../../models/InstagramData';
+import InstagramData from '../../models/InstagramData';
 import { getLongLivedAccessToken } from '../../services/instagramGraphAPI';
-import axios from 'axios';
 
-//TODO: Create a way to obtain all objectID and loop through them to refresh the token every x days
-//TODO: When refreshing token, we should also refresh the user's basic info / insights info for changed data
-
-const tokenRefresh = async (objectID: Object) => {
+const tokenRefresh = async () => {
     try {
-        const user = await instagram_data.findById(objectID);
-        const oldLongToken = user.longLivedAccessToken;
+        const tenDaysAgo = new Date();
+        tenDaysAgo.setDate(tenDaysAgo.getDate() - 10);
 
-        const newLongtoken = await getLongLivedAccessToken(oldLongToken);
+        const users = await InstagramData.find({ tokenCreationDate: { $lt: tenDaysAgo } });
 
-        const testObject2 = await instagram_data.findOneAndUpdate(
-            { _id: objectID }, // filter
-            { $set: { longLivedAccessToken: newLongtoken } }, // update
-            { new: true } // options to return new user object
-        );
+        console.log(`Number of users being updated: ${users.length}`);
 
-        return testObject2;
+        for (let user of users) {
+            const oldLongToken = user.longLivedAccessToken;
+            const newLongtoken = await getLongLivedAccessToken(oldLongToken);
+
+            await InstagramData.findOneAndUpdate(
+                { _id: user._id },
+                { $set: { longLivedAccessToken: newLongtoken, tokenCreationDate: new Date() } }, // Update token and tokenCreationDate
+                { new: true }
+            );
+        }
+
+        return "Tokens refreshed successfully";
     } catch (error) {
         console.log(error);
         return error;
