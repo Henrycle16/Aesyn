d8cabbeaa2993829b45eaa18c201bc47a8df0e71
Calvin Nguyen <cnguyenr42@gmail.com>
2024-05-10 17:54:01 -0700

    Add niche selection limit logic

--- client/src/components/InitialSignupQuestions/CreatorSignup/NicheSelect.tsx
+++ client/src/components/InitialSignupQuestions/CreatorSignup/NicheSelect.tsx
@@ -11,18 +11,32 @@ interface NicheSelectProps {
 
 // Niche Options List
 const nichesArray = [
-  { key: 0, label: "Actor" },
-  { key: 1, label: "Adventure & Outdoors" },
-  { key: 2, label: "Animals & Pets" },
-  { key: 3, label: "Art & Photography" },
-  { key: 4, label: "Athlete & Sports" },
-  { key: 5, label: "Automotive" },
-  { key: 6, label: "Beauty" },
-  { key: 7, label: "Celebrity & Public Figure" },
+  { key: 0, label: "Lifestyle" },
+  { key: 1, label: "Fashion" },
+  { key: 2, label: "Beauty" },
+  { key: 3, label: "Travel" },
+  { key: 4, label: "Health & Fitness" },
+  { key: 5, label: "Food & Drink" },
+  { key: 6, label: "Model" },
+  { key: 7, label: "Art & Photography" },
   { key: 8, label: "Comedy & Entertainment" },
-  { key: 9, label: "Education" },
-  { key: 10, label: "OnlyFans" },
-  { key: 11, label: "LGBTQ" },
+  { key: 9, label: "Music & Dance" },
+  { key: 10, label: "Entrepreneur & Business" },
+  { key: 11, label: "Family & Children" },
+  { key: 12, label: "Animals & Pets" },
+  { key: 13, label: "Education" },
+  { key: 14, label: "Adventure & Outdoors" },
+  { key: 15, label: "Athlete & Sports" },
+  { key: 16, label: "Technology" },
+  { key: 17, label: "Gaming" },
+  { key: 18, label: "Celebrity & Public Figure" },
+  { key: 19, label: "Actor" },
+  { key: 20, label: "Healthcare" },
+  { key: 21, label: "LGBTQ2+" },
+  { key: 22, label: "Vegan" },
+  { key: 23, label: "Cannabis" },
+  { key: 24, label: "Skilled Trades" },
+  { key: 25, label: "Automotive" },
 ];
 
 const ContactForm = ({
@@ -50,7 +64,11 @@ const ContactForm = ({
           {nichesArray.map((data) => (
             <Chip
               key={data.key}
-              onClick={() => handleNicheChange(data.label)}
+              onClick={() => {
+                if (formData.niche.length < 6 || formData.niche.includes(data.label)) {
+                  handleNicheChange(data.label)
+                }
+              }}
               variant={
                 formData.niche.includes(data.label) ? "filled" : "outlined"
               }
