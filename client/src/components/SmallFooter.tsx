e61a1669ec4d2b63d486b376b27d5398dcc67994
Henry <91310711+SX129@users.noreply.github.com>
2024-05-27 16:44:29 -0500

    merge Feature/footer branch into main (#31)
    * initial commit for landing page footer
    
    * initial commit for small footer
    
    * changed viewing height of main section

---
+++ client/src/components/SmallFooter.tsx
@@ -0,0 +1,14 @@
+const SmallFooter = () => {
+    return (
+      <footer className="bg-[#E5E6EB]">
+          <div className="text-[#44A4A] text-center py-2">
+            <span className="text-xs">
+              © 2024 All Rights Reserved by Start Up LLC
+            </span>
+          </div>
+      </footer>
+    );
+  };
+  
+  export default SmallFooter;
+  
