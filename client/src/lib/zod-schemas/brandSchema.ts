9d93271513025f15d87fbaa38723305f0c2d1b6d
Calvin Nguyen <cnguyenr42@gmail.com>
2024-06-01 16:52:15 -0700

    Add phone number validation to require only integers

--- client/src/lib/zod-schemas/brandSchema.ts
+++ client/src/lib/zod-schemas/brandSchema.ts
@@ -5,5 +5,5 @@ export const FormDataSchema = z.object({
   companyName: z.string().min(1, "Company name is required"),
   firstName: z.string().min(1, "First name is required").refine((data) => validator.isAlpha(data), "No numbers or special characters"),
   lastName: z.string().min(1, "Last name is required").refine((data) => validator.isAlpha(data), "No numbers or special characters"),
-  contactPhoneNumber: z.string().min(10, "Valid phone number required"),
+  contactPhoneNumber: z.string().min(10, "Valid phone number required").refine((data) => validator.isInt(data), "Valid phone number required"),
 })
