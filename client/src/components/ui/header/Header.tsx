a3fa5d23085bfc0eefd9b7b3c773ebee640c981e
Calvin Nguyen <cnguyenr42@gmail.com>
2024-06-09 13:51:20 -0700

    Expanded width of ProfileHeader.tsx

--- client/src/components/ui/header/Header.tsx
+++ client/src/components/ui/header/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex flex-wrap px-16 pt-4 flex-col md:flex-row items-center">
+    <header className="flex flex-wrap px-16 pt-5 flex-col md:flex-row items-center">
       <Link href={"/"} className="text-2xl text-gray-900 font-semibold">
         ShareFluence
       </Link>
--- client/src/components/ui/header/ProfileHeader.tsx
+++ client/src/components/ui/header/ProfileHeader.tsx
@@ -4,15 +4,15 @@ import CreatorAvatar from "./creator/CreatorAvatar";
 
 const ProfileHeader = () => {
   return (
-    <header className="container mx-auto flex flex-wrap px-5 pt-4 flex-col md:flex-row items-center">
+    <header className="flex flex-wrap px-16 pt-4 flex-col md:flex-row items-center">
       <Link href={"/"} className="text-2xl text-gray-900 font-semibold">
         ShareFluence
       </Link>
       <div className="flex ml-40 items-center">
-      <ThreeMenu />
+        <ThreeMenu />
       </div>
       <div className="flex ml-auto items-center">
-      <CreatorAvatar />
+        <CreatorAvatar />
       </div>
     </header>
   );
