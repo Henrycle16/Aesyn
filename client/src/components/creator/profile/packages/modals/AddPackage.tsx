86e1e19f179538349e6bbd89a1be9b740922abcb
Calvin Nguyen <cnguyenr42@gmail.com>
2024-06-11 13:30:23 -0700

    Refactor Package Model layout

--- client/src/components/creator/profile/packages/modals/AddPackage.tsx
+++ client/src/components/creator/profile/packages/modals/AddPackage.tsx
@@ -1,109 +1,111 @@
 "use client";
 
-import Button from "@mui/material/Button";
-
 type Props = {};
 
 const AddPackage = (props: Props) => {
   return (
     <dialog id="add_package_modal" className="modal">
-      <div className="modal-box bg-white text-[#4A4A4A] min-w-[60rem] pt-10 pl-14 pr-10 pb-2">
+      <div className="modal-box bg-white text-[#4A4A4A] min-w-[60rem] pt-10 pl-14 pr-10 pb-8">
         {/* Header Text */}
         <div className="">
-          <h1 className="text-[#184465] font-semibold text-2xl">Add New Package</h1>
-          <p className="pb-4 pt-2 text-sm">Create your content package plan to list on your profile for brands to purchase.</p>
+          <h1 className="text-[#184465] font-semibold text-2xl">
+            Add New Package
+          </h1>
+          <p className="pb-4 pt-2 text-sm">
+            Create your content package plan to list on your profile for brands
+            to purchase.
+          </p>
         </div>
-        {/* Input Fields Container */}
-        <div className="grid grid-cols-2 gap-x-12 gap-y-7">
-          {/* Social Media Select */}
-          <div>
-            <label htmlFor="social_media" className="block font-bold">
-              *Social Media
-            </label>
-            <select
-              id="social_media"
-              name="social_media"
-              className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            >
-              <option>[Select]</option>
-              <option>Twitter</option>
-              <option>Facebook</option>
-            </select>
-          </div>
-          {/* Social Media Select */}
-          <div>
-            <label htmlFor="package_type" className="block font-bold">
-              *Package Type
-            </label>
-            <select
-              id="package_type"
-              name="package_type"
-              className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            >
-              <option>[Select]</option>
-            </select>
-          </div>
-          {/* Description */}
-          <div className="">
-            <label htmlFor="description" className="block font-bold">
-              Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              placeholder="Enter package description here..."
-              maxLength={100}
-              rows={4}
-              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            ></textarea>
-          </div>
-          {/* Qty and Price Container */}
-          <div className="flex gap-12">
-            {/* Quantiy */}
+        {/* Form */}
+        <form method="dialog">
+          {/* Input Fields Container */}
+          <div className="grid grid-cols-2 gap-x-12 gap-y-7">
+            {/* Social Media Select */}
             <div>
-              <label htmlFor="qty" className="block font-bold">
-                *Quantity
+              <label htmlFor="social_media" className="block font-bold">
+                *Social Media
               </label>
-              <input 
-                type="text" 
-                id="qty" 
-                name="qty"
-                placeholder="#"
-                className="w-full mt-1 block py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-              />
+              <select
+                id="social_media"
+                name="social_media"
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              >
+                <option>[Select]</option>
+                <option>Twitter</option>
+                <option>Facebook</option>
+              </select>
             </div>
-            {/* Price */}
+            {/* Social Media Select */}
             <div>
-              <label htmlFor="price" className="block font-bold">
-                *Price (USD)
+              <label htmlFor="package_type" className="block font-bold">
+                *Package Type
               </label>
-              <input 
-                type="number" 
-                id="price" 
-                name="price"
-                className="w-full mt-1 block py-3 px-3 pl-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-              />
-              <span className="absolute right-auto top-[54.5%] ml-4">$</span>
+              <select
+                id="package_type"
+                name="package_type"
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              >
+                <option>[Select]</option>
+              </select>
+            </div>
+            {/* Description */}
+            <div className="">
+              <label htmlFor="description" className="block font-bold">
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                placeholder="Enter package description here..."
+                maxLength={100}
+                rows={4}
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              ></textarea>
+            </div>
+            {/* Qty and Price Container */}
+            <div className="flex gap-12">
+              {/* Quantiy */}
+              <div>
+                <label htmlFor="qty" className="block font-bold">
+                  *Quantity
+                </label>
+                <input
+                  type="text"
+                  id="qty"
+                  name="qty"
+                  placeholder="#"
+                  className="w-full mt-1 block py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+                />
+              </div>
+              {/* Price */}
+              <div>
+                <label htmlFor="price" className="block font-bold">
+                  *Price (USD)
+                </label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  className="w-full mt-1 block py-3 px-3 pl-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+                />
+                <span className="absolute right-auto top-[54.5%] ml-4">$</span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* Action Buttons */}
-        <div className="flex justify-end mt-14">
-          <Button
-            onClick={() => console.log("Save Package")}
-            type="button"
-            variant="contained"
-            className="bg-[#3798E3] py-2 px-7 capitalize font-medium rounded-md"
-          >
-            Save
-          </Button>
-        </div>
-        <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-lg btn-circle btn-ghost outline-none absolute right-4 top-2 text-lg">✕</button>
-          </form>
-        </div>
+          {/* Action Buttons -- if there is a button in form, it will close the modal */}
+          <div className="flex justify-end mt-14">
+            <button
+              onClick={() => console.log("Save Package")}
+              // type="submit"
+              className="bg-[#3798E3] text-white font-medium py-2 px-7 capitalize rounded-md hover:bg-[#2C7AB6]"
+            >
+              Save
+            </button>
+          </div>
+          <button className="btn btn-lg btn-circle btn-ghost outline-none absolute right-4 top-2 text-lg">
+            ✕
+          </button>
+        </form>
       </div>
     </dialog>
   );
--- client/src/components/creator/profile/packages/modals/EditPackage.tsx
+++ client/src/components/creator/profile/packages/modals/EditPackage.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Button from "@mui/material/Button";
-import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 
 type Props = {
   id: number;
@@ -9,130 +8,133 @@ type Props = {
   packageType: string;
   packageDescription: string;
   price: number;
-}
+};
 
 const EditPackage = (props: Props) => {
   return (
     <dialog id={`edit_package_modal_${props.id}`} className="modal">
-      <div className="modal-box bg-white text-[#4A4A4A] min-w-[60rem] pt-10 pl-14 pr-10 pb-2">
+      <div className="modal-box bg-white text-[#4A4A4A] min-w-[60rem] pt-10 pl-14 pr-10 pb-8">
         {/* Header Text */}
         <div className="">
           <div className="flex gap-3 items-center">
-            <h1 className="text-[#184465] font-semibold text-2xl">Edit Package</h1>
+            <h1 className="text-[#184465] font-semibold text-2xl">
+              Edit Package
+            </h1>
             <DeleteOutlineIcon
               sx={{ color: "#FF0000" }}
               className="cursor-pointer"
               onClick={() => console.log("Delete Package")}
             />
           </div>
-          <p className="pb-4 pt-2 text-sm">Edit your content package plan to list on your profile for brands to purchase.</p>
+          <p className="pb-4 pt-2 text-sm">
+            Edit your content package plan to list on your profile for brands to
+            purchase.
+          </p>
         </div>
-        {/* Input Fields Container */}
-        <div className="grid grid-cols-2 gap-x-12 gap-y-7">
-          {/* Social Media Select */}
-          <div>
-            <label htmlFor="social_media" className="block font-bold">
-              *Social Media
-            </label>
-            <select
-              id={`social_media_${props.id}`}
-              name="social_media"
-              // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
-              defaultValue={props.socialMedia}
-              className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            >
-              <option value="">[Select]</option>
-              <option value="Instagram">Instagram</option>
-              <option value="Twitter">Twitter</option>
-              <option value="Facebook">Facebook</option>
-            </select>
-          </div>
-          {/* Social Media Select */}
-          <div>
-            <label htmlFor="package_type" className="block font-bold">
-              *Package Type
-            </label>
-            <select
-              id={`package_type_${props.id}`}
-              name="package_type"
-              // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
-              defaultValue={props.packageType}
-              className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            >
-              <option>[Select]</option>
-              <option value="Reel Post">Reel Post</option>
-              <option value="Photo Post">Photo Post</option>
-              <option value="Multi-Photo Post">Multi-Photo Post</option>
-            </select>
-          </div>
-          {/* Description */}
-          <div className="">
-            <label htmlFor="description" className="block font-bold">
-              Description
-            </label>
-            <textarea
-              id={`description_${props.id}`}
-              name="description"
-              placeholder="Enter package description here..."
-              maxLength={100}
-              rows={4}
-              // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
-              defaultValue={props.packageDescription}
-              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-            ></textarea>
-          </div>
-          {/* Qty and Price Container */}
-          <div className="flex gap-12">
-            {/* Quantiy */}
-            <div className="flex-1">
-              <label htmlFor="qty" className="block font-bold">
-                *Quantity
+        <form method="dialog">
+          {/* Input Fields Container */}
+          <div className="grid grid-cols-2 gap-x-12 gap-y-7">
+            {/* Social Media Select */}
+            <div>
+              <label htmlFor="social_media" className="block font-bold">
+                *Social Media
               </label>
-              <input 
-                type="text" 
-                id={`qty_${props.id}`} 
-                name="qty"
-                placeholder="#"
-                className="w-full mt-1 block py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-              />
-            </div>
-            {/* Price */}
-            <div className="flex-1">
-              <label htmlFor="price" className="block font-bold">
-                *Price (USD)
-              </label>
-              <input
-                type="number" 
-                id={`price_${props.id}`} 
-                name="price"
+              <select
+                id={`social_media_${props.id}`}
+                name="social_media"
                 // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
-                defaultValue={props.price}
-                className="w-full mt-1 block py-3 px-3 pl-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
-              />
-              <span className="absolute right-auto top-[54.5%] ml-4">$</span>
+                defaultValue={props.socialMedia}
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              >
+                <option value="">[Select]</option>
+                <option value="Instagram">Instagram</option>
+                <option value="Twitter">Twitter</option>
+                <option value="Facebook">Facebook</option>
+              </select>
+            </div>
+            {/* Social Media Select */}
+            <div>
+              <label htmlFor="package_type" className="block font-bold">
+                *Package Type
+              </label>
+              <select
+                id={`package_type_${props.id}`}
+                name="package_type"
+                // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
+                defaultValue={props.packageType}
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              >
+                <option>[Select]</option>
+                <option value="Reel Post">Reel Post</option>
+                <option value="Photo Post">Photo Post</option>
+                <option value="Multi-Photo Post">Multi-Photo Post</option>
+              </select>
+            </div>
+            {/* Description */}
+            <div className="">
+              <label htmlFor="description" className="block font-bold">
+                Description
+              </label>
+              <textarea
+                id={`description_${props.id}`}
+                name="description"
+                placeholder="Enter package description here..."
+                maxLength={100}
+                rows={4}
+                // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
+                defaultValue={props.packageDescription}
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+              ></textarea>
+            </div>
+            {/* Qty and Price Container */}
+            <div className="flex gap-12">
+              {/* Quantiy */}
+              <div className="flex-1">
+                <label htmlFor="qty" className="block font-bold">
+                  *Quantity
+                </label>
+                <input
+                  type="text"
+                  id={`qty_${props.id}`}
+                  name="qty"
+                  placeholder="#"
+                  className="w-full mt-1 block py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+                />
+              </div>
+              {/* Price */}
+              <div className="flex-1">
+                <label htmlFor="price" className="block font-bold">
+                  *Price (USD)
+                </label>
+                <input
+                  type="number"
+                  id={`price_${props.id}`}
+                  name="price"
+                  // ! Should not used defaultValue, use value instead with onChange. (Only for static data, defaultValue is used.)
+                  defaultValue={props.price}
+                  className="w-full mt-1 block py-3 px-3 pl-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-[#3798E3] focus:border-[#3798E3] sm:text-sm"
+                />
+                <span className="absolute right-auto top-[54.5%] ml-4">$</span>
+              </div>
             </div>
           </div>
-        </div>
-        {/* Action Buttons */}
-        <div className="flex justify-end mt-14">
-          <Button
-            onClick={() => console.log("Save Package")}
-            type="button"
-            variant="contained"
-            className="bg-[#3798E3] py-2 px-7 capitalize font-medium rounded-md"
-          >
-            Save
-          </Button>
-        </div>
-        <div className="modal-action">
-          <form method="dialog">
-            {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-lg btn-circle btn-ghost outline-none absolute right-4 top-2 text-lg">✕</button>
-          </form>
-        </div>
+          {/* Action Buttons -- if there is a button in form, it will close the modal */}
+          <div className="flex justify-end mt-14">
+            <button
+              onClick={() => console.log("Save Package")}
+              // type="submit"
+              className="bg-[#3798E3] text-white py-2 px-7 capitalize font-medium rounded-md hover:bg-[#2C7AB6]"
+            >
+              Save
+            </button>
+          </div>
+          <button className="btn btn-lg btn-circle btn-ghost outline-none absolute right-4 top-2 text-lg">
+            ✕
+          </button>
+        </form>
       </div>
     </dialog>
-  )
-}
+  );
+};
 
-export default EditPackage
+export default EditPackage;
