f720e60d53ec02f10287f5461cc90675b469ddf1
Henry Le <henrycle16@gmail.com>
2024-06-15 18:05:11 -0500

    Finishing Top Component

--- client/src/components/creator/profile/main/Bio.tsx
+++ client/src/components/creator/profile/main/Bio.tsx
@@ -1,9 +1,17 @@
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+
 const Bio = () => {
   return (
     <div className="flex-1">
-      Bio
+      <div className="flex justify-start">
+        <h1 className="text-2xl font-semibold text-[#184465]">Bio</h1>
+        <ModeEditOutlineOutlinedIcon
+          sx={{ color: "#3798E3", fontSize: 25 }}
+          className="border-2 border-[#3798E3] rounded-full p-[.12rem] cursor-pointer ml-3 mt-1"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Bio
+export default Bio;
--- client/src/components/creator/profile/main/SocialMedia.tsx
+++ client/src/components/creator/profile/main/SocialMedia.tsx
@@ -1,7 +1,15 @@
+import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+
 const SocialMedia = () => {
   return (
     <div className="flex-[1.25]">
-      Social Media
+      <div className="flex justify-start">
+        <h1 className="text-2xl font-semibold text-[#184465]">Social Media </h1>
+        <ModeEditOutlineOutlinedIcon
+          sx={{ color: "#3798E3", fontSize: 25 }}
+          className="border-2 border-[#3798E3] rounded-full p-[.12rem] cursor-pointer ml-3 mt-1"
+        />
+      </div>
     </div>
   )
 }
