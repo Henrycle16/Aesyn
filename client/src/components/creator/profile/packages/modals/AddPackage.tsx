7c2a56cfa39b4e554e08c3bb8a00f9213a3de68c
Calvin Nguyen <77700030+calvinguyen@users.noreply.github.com>
2024-06-16 14:45:59 -0700

    UI/profile packages (#46)
    * Change default font to Figma design font
    
    * Create packages layout
    
    * Make Packages scrollable
    
    * Add Instagram SVG to PackageCards
    
    * Refactor PackageCard Text Layout
    
    * Remove unneccessary css
    
    * Add styling to packages scrollbar
    
    * Create Add Package Model Skeleton
    
    * Create Edit Package Modal Skeleton
    
    * Change 'X' button to '✕'
    
    * Refactor Package Model layout
    
    * Add Delete Package Modal and change button styling padding and font weight to match figma
    
    * Adjust $ sign position to align with price input value
    
    * Change form field colors to match figma design
    
    * Remove unnecessary css styling from input fields
    
    * Reduce vertical gap between input fields
    
    * Replace Add Package Card with Button
    
    * Minor refactoring
    
    * Add Quantity to packages model

---
+++ client/src/components/creator/profile/packages/modals/AddPackage.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+type Props = {};
+
+const AddPackage = (props: Props) => {
+  return (
+    <dialog id="add_package_modal" className="modal">
+      <div className="modal-box bg-white text-[#061119] min-w-[60rem] pt-10 pl-14 pr-10 pb-8">
+        {/* Header Text */}
+        <div className="">
+          <h1 className="text-[#184465] font-semibold text-2xl">
+            Add New Package
+          </h1>
+          <p className="pb-4 pt-2 text-sm">
+            Create your content package plan to list on your profile for brands
+            to purchase.
+          </p>
+        </div>
+        {/* Form */}
+        <form method="dialog">
+          {/* Input Fields Container */}
+          <div className="grid grid-cols-2 gap-x-10 gap-y-7">
+            {/* Social Media Select */}
+            <div>
+              <label htmlFor="social_media" className="text-[#4A4A4A] block font-bold">
+                *Social Media
+              </label>
+              <select
+                id="social_media"
+                name="social_media"
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:border-[#3798E3] sm:text-sm"
+              >
+                <option>[Select]</option>
+                <option>Twitter</option>
+                <option>Facebook</option>
+              </select>
+            </div>
+            {/* Social Media Select */}
+            <div>
+              <label htmlFor="package_type" className="text-[#4A4A4A] block font-bold">
+                *Package Type
+              </label>
+              <select
+                id="package_type"
+                name="package_type"
+                className="mt-1 block w-full py-3 px-3 border border-gray-300 bg-white rounded-md focus:outline-none focus:border-[#3798E3] sm:text-sm"
+              >
+                <option>[Select]</option>
+              </select>
+            </div>
+            {/* Description */}
+            <div className="">
+              <label htmlFor="description" className="text-[#4A4A4A] block font-bold">
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                placeholder="Enter package description here..."
+                maxLength={100}
+                rows={4}
+                className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-[#3798E3] sm:text-sm"
+              ></textarea>
+            </div>
+            {/* Qty and Price Container */}
+            <div className="flex gap-12">
+              {/* Quantiy */}
+              <div>
+                <label htmlFor="qty" className="text-[#4A4A4A] block font-bold">
+                  *Quantity
+                </label>
+                <input
+                  type="text"
+                  id="qty"
+                  name="qty"
+                  placeholder="#"
+                  className="w-full mt-1 block py-3 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-[#3798E3] sm:text-sm"
+                />
+              </div>
+              {/* Price */}
+              <div>
+                <label htmlFor="price" className="text-[#4A4A4A] block font-bold">
+                  *Price (USD)
+                </label>
+                <input
+                  type="number"
+                  id="price"
+                  name="price"
+                  className="w-full mt-1 block py-3 px-3 pl-6 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:border-[#3798E3] sm:text-sm"
+                />
+                <span className="absolute right-auto top-[54%] ml-4 text-sm">$</span>
+              </div>
+            </div>
+          </div>
+          {/* Action Buttons -- if there is a button in form, it will close the modal */}
+          <div className="flex justify-end mt-14">
+            <button
+              onClick={() => console.log("Save Package")}
+              // type="submit"
+              className="bg-[#3798E3] text-white font-bold py-3 px-6 capitalize rounded-md hover:bg-[#2C7AB6]"
+            >
+              Save
+            </button>
+          </div>
+          <button className="btn btn-lg btn-circle btn-ghost outline-none absolute right-4 top-2 text-lg">
+            ✕
+          </button>
+        </form>
+      </div>
+    </dialog>
+  );
+};
+
+export default AddPackage;
