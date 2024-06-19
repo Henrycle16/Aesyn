173e248c375f20d1ee6a0c970f81933e0125a6df
Justin Talictic <jaytalictic@email.com>
2024-06-19 18:18:46 -0500

    login form reverted from mui

--- client/src/components/user/Login.tsx
+++ client/src/components/user/Login.tsx
@@ -21,7 +21,6 @@ import { AppDispatch } from "@/redux/store";
 import { useDispatch } from "react-redux";
 import { useAppSelector } from "@/redux/store";
 
-
 const LoginComponent = () => {
   const dispatch = useDispatch<AppDispatch>();
   const session = useSession();
@@ -52,105 +51,90 @@ const LoginComponent = () => {
   const onClickLogOut = () => {
     dispatch(logOut());
     signOut({ redirect: false });
-  }
+  };
 
   return (
-    <Container 
-    className="flex flex-col border border-gray-300 rounded-2xl md:px-20 px-5 gap-1"
-    component="main" 
-    maxWidth="sm">
+    <div>
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        className="flex flex-col border border-gray-300 rounded-2xl md:px-20 px-5 gap-1"
+      >
+        <div className="mt-8 flex flex-col items-center">
+          <Avatar className="m-1 bg-blue-500">
+            <PersonPinOutlinedIcon />
+          </Avatar>
+          <h1 className="text-2xl">Login</h1>
+        </div>
+        <div className="mt-5">
+          <input
+            className="input-md w-full"
+            type="text"
+            placeholder="Email Address"
+            id="email"
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setEmail(e.target.value)
+            }
+            name="email"
+            autoFocus
+            autoComplete="email"
+            required
+          />
+        </div>
+        <div>
+          <input
+            className="input-md w-full"
+            type="password"
+            placeholder="Password"
+            id="password"
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setPassword(e.target.value)
+            }
+            name="password"
+            autoFocus
+            autoComplete="new-password"
+            required
+          />
+        </div>
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          className="mt-3 mb-2 btn-primary-color"
+        >
+          Login
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
+          className="mt-3 mb-2"
+          onClick={onClickLogOut}
+        >
+          Sign Out
+        </Button>
+
+        <button
+          onClick={() => document.getElementById("sign-up-modal").showModal()}
+        >
+          <Link href="#">Don&apos;t have an account? Sign up</Link>
+        </button>
+
+        <div className="flex pt-16 pb-1">
+          <p className="mx-auto text-sm text-gray-500">
+            Copyright ©{" "}
+            <Link href={"/"} className="text-gray-500">
+              ShareFluence
+            </Link>{" "}
+            {new Date().getFullYear()}.
+          </p>
+        </div>
+      </form>
       <SignUpModal>
         <SignUpPopup />
       </SignUpModal>
-      <Box className="mt-8 flex flex-col items-center">
-        <Avatar className="m-1 bg-blue-500">
-          <PersonPinOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box
-          className="mt-3"
-          component="form"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <Grid container spacing={2} className="pb-6">
-            <Grid item xs={12}>
-              <input
-                className="input-md border border-gray-300 w-full focus:outline-none focus:ring-1 focus:ring-inset focus:ring-[#3798e3] focus:border-[#3798e3] focus:bg-neutral-100 rounded-md"
-                type="text"
-                placeholder="Email Address"
-                id="email"
-                value={email}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setEmail(e.target.value)
-                }
-                name="email"
-                autoFocus
-                autoComplete="email"
-                required
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <input
-                className="input-md border border-gray-300 w-full focus:outline-none focus:ring-1 focus:ring-inset focus:ring-[#3798e3] focus:border-[#3798e3] focus:bg-neutral-100 rounded-md"
-                type="password"
-                placeholder="Password"
-                id="password"
-                value={password}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setPassword(e.target.value)
-                }
-                name="password"
-                autoFocus
-                autoComplete="new-password"
-                required
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="mt-3 mb-2 bg-muiblue"
-          >
-            Login
-          </Button>
-          <Button
-            fullWidth
-            variant="contained"
-            className="mt-3 mb-2 bg-muiblue"
-            onClick={onClickLogOut}
-          >
-            Sign Out
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Grid item>
-              <button
-                onClick={() =>
-                  document.getElementById("sign-up-modal").showModal()
-                }
-              >
-                <Link href="#">Don&apos;t have an account? Sign up</Link>
-              </button>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-      <Typography
-        variant="body2"
-        color="text.secondary"
-        align="center"
-        sx={{ paddingTop: "20px" }}
-      >
-        {"Copyright © "}
-        <Link color="inherit" href="http://github.com/H2JC/H2JC">
-          H2JC
-        </Link>{" "}
-        {new Date().getFullYear()}
-        {"."}
-      </Typography>
-    </Container>
+    </div>
   );
 };
 
