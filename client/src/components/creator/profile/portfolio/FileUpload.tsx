1890cdd66fb61966bb290908e2b8d833950bbbff
Henry Nguyen <odyzetx@gmail.com>
2024-06-27 18:19:01 -0500

    cleaned up code

--- client/src/components/creator/profile/portfolio/FileUpload.tsx
+++ client/src/components/creator/profile/portfolio/FileUpload.tsx
@@ -1,32 +1,12 @@
 "use client";
-//import { uploadToS3 } from "@/lib/s3";
-import { useMutation } from "@tanstack/react-query";
 import Upload from "@/components/ui/svgs/Upload";
-import { Inbox, Loader2 } from "lucide-react";
+import { Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
-import axios from "axios";
 import { toast } from "react-hot-toast";
-import { useRouter } from "next/navigation";
 
 const FileUpload = () => {
-  const router = useRouter();
   const [uploading, setUploading] = React.useState(false);
-  // const { mutate, isPending } = useMutation({
-  //   mutationFn: async ({
-  //     file_key,
-  //     file_name,
-  //   }: {
-  //     file_key: string;
-  //     file_name: string;
-  //   }) => {
-  //     const response = await axios.post("/api/create-chat", {
-  //       file_key,
-  //       file_name,
-  //     });
-  //     return response.data;
-  //   },
-  // });
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: {
@@ -44,29 +24,6 @@ const FileUpload = () => {
         toast.error("File too large.");
         return;
       }
-
-      // try {
-      //   setUploading(true);
-      //   const data = await uploadToS3(file);
-      //   if (!data?.file_key || !data.file_name) {
-      //     toast.error("Something went wrong.");
-      //     return;
-      //   }
-      //   mutate(data, {
-      //     onSuccess: ({ chat_id }) => {
-      //       toast.success("Chat created!");
-      //       router.push(`/chat/${chat_id}`);
-      //     },
-      //     onError: (error) => {
-      //       toast.error("Error creating chat.");
-      //       console.error(error);
-      //     },
-      //   });
-      // } catch (error) {
-      //   console.log(error);
-      // } finally {
-      //   setUploading(false);
-      // }
     },
   });
 
