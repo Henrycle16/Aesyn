9b6d1483c96ceaf4a611f65a7da19ce10e124a3b
Henry Le <henrycle16@gmail.com>
2024-07-18 00:39:39 -0500

    Implement ImageCropper into Profile Card. Still might need some more stuff to make it work better

--- client/src/components/creator/profile/main/ProfileCard.tsx
+++ client/src/components/creator/profile/main/ProfileCard.tsx
@@ -5,9 +5,8 @@ import Badge from "@mui/material/Badge";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import VerifiedUserOutlinedIcon from "@mui/icons-material/VerifiedUserOutlined";
-import FileUpload from "../portfolio/FileUpload";
 import React, { useRef, useState } from "react";
-import Image from "next/legacy/image";
+import ImageCropper from "@/components/utils/ImageCropper/ImageCropper";
 
 interface Avatar {
   uri: string;
@@ -15,56 +14,24 @@ interface Avatar {
 }
 
 const ProfileCard = () => {
-  const [avatar, setAvatar] = useState<Avatar | null>(null);
-  const [tempAvatar, setTempAvatar] = useState<Avatar | null>(null);
-  const [previewUrl, setPreviewUrl] = useState<string | null>(null);
-  const [showFileUpload, setShowFileUpload] = useState(false);
-  const fileInputRef = useRef<HTMLInputElement>(null);
+  const avatarUrl = useRef(
+    "/static/images/avatar/1.jpg"
+  );
+  const [modalOpen, setModalOpen] = useState(false);
 
-
-  const handleFileUpload = ({ uri, name }: Avatar) => {
-    setTempAvatar({ uri, name });
-    setPreviewUrl(uri);
+  const updateAvatar = (imgSrc: string) => {
+    avatarUrl.current = imgSrc;
+    setModalOpen(false);
   };
 
   const openModal = () => {
-    setTempAvatar(avatar);
-    setPreviewUrl(avatar?.uri || null);
-    setShowFileUpload(false);
+    setModalOpen(true);
     (document.getElementById("avatar_modal") as HTMLDialogElement).showModal();
   };
 
-  const onFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (tempAvatar) {
-      setAvatar(tempAvatar);
-      setPreviewUrl(null);
-    }
-    (document.getElementById(`avatar_modal`) as HTMLDialogElement).close();
-  };
-
   const closeModal = () => {
-    setPreviewUrl(null);
+    setModalOpen(false);
     (document.getElementById(`avatar_modal`) as HTMLDialogElement).close();
-  };
-
-  const handleChangeImageClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        if (reader.result) {
-          handleFileUpload({ uri: reader.result as string, name: file.name });
-        }
-      };
-      reader.readAsDataURL(file);
-    }
   };
 
   return (
@@ -86,11 +53,8 @@ const ProfileCard = () => {
                 }
               >
                 <Avatar
-                  alt={avatar?.name || "Default Name"}
-                  src={
-                    avatar?.uri ||
-                    "https://avatarfiles.alphacoders.com/161/161002.jpg"
-                  }
+                  alt="Avatar"
+                  src={avatarUrl.current}
                   sx={{ width: 150, height: 150 }}
                 />
               </Badge>{" "}
@@ -146,7 +110,7 @@ const ProfileCard = () => {
 
       {/* Modal */}
       <dialog id="avatar_modal" className="modal">
-        <div className="modal-box bg-white text-[#061119] min-w-[40rem] min-h-[31.1rem] pt-8 px-10 pb-6 flex flex-col">
+        <div className="modal-box bg-white text-[#061119] min-w-[42rem] min-h-[32.1rem] pt-8 px-10 pb-6 flex flex-col">
           <div className="flex flex-col mr-auto">
             <h1 className="heading-text-color heading1">Profile</h1>
             <h2 className="py-2 gc-label-color body1">
@@ -156,54 +120,13 @@ const ProfileCard = () => {
 
           {/* Upload box */}
           <form
-            onSubmit={onFormSubmit}
             method="dialog"
             className="flex flex-col flex-1"
           >
             <div className="flex flex-1">
-              <div className="pt-6 px-24 w-full">
-                {previewUrl ? (
-                  <div className="relative w-[100%] h-[264px] bg-gray-50 ">
-                    <Image
-                      src={previewUrl}
-                      alt="image"
-                      width={368}
-                      height={264}
-                      objectFit="cover"
-                      className="rounded"
-                    />
-                  </div>
-                ) : (
-                  <FileUpload
-                    paddingY="py-28"
-                    onFileUpload={handleFileUpload}
-                  />
-                )}
+              <div className="pt-6 px-20 w-full">
+                <ImageCropper updateAvatar={updateAvatar} />
               </div>
-            </div>
-
-            <div className="flex justify-end mt-8">
-              {previewUrl && (
-                <button
-                  type="button"
-                  onClick={handleChangeImageClick}
-                  className="border-solid border-2 border-[#3798E3] text-[#3798E3] font-semibold py-2 px-6 rounded-md flex items-center justify-center mr-3 hover:bg-[#F5F5F5]"
-                >
-                  Change Image
-                </button>
-              )}
-              <input
-                type="file"
-                ref={fileInputRef}
-                style={{ display: "none" }}
-                onChange={handleFileInputChange}
-              />
-              <button
-                type="submit"
-                className="bg-[#3798E3] text-white font-bold py-3 px-7 capitalize rounded-md hover:bg-[#2C7AB6]"
-              >
-                Save
-              </button>
             </div>
             <button
               onClick={closeModal}
