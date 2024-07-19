574cbf08b1ee7c951fe017299464cf0ffa6868e0
Calvin Nguyen <cnguyenr42@gmail.com>
2024-07-18 18:06:53 -0700

    Add back get creator by username endpoint

--- server/src/routes/creatorRoute.ts
+++ server/src/routes/creatorRoute.ts
@@ -186,6 +186,21 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route   Get api/creators
+// @desc    check if username exist
+// @access  Public -> Private
+router.get("/username/:username", async (req, res) => {
+  try {
+    const creator = await Creator.findOne({ 
+      userName: req.params.username 
+    }).populate("user", ["firstName", "lastName", "avatar", "email"]);
+    console.log(creator);
+    res.status(200).json(creator);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 // @route   DELETE api/creators
 // @desc    Delete Creators profile, user, & posts
 // @access  Private
