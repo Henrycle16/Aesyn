846e72b1532c51f6e0efada3c34c2beb35834ea8
Justin Talictic <jaytalictic@email.com>
2024-07-23 16:29:13 -0500

    Login Fix

--- client/src/actions/authApi.ts
+++ client/src/actions/authApi.ts
@@ -3,12 +3,15 @@ import api from "./axiosConfig";
 // Login User
 // Public
 const login = async (email: string, password: string) => {
- const body = JSON.stringify({ email, password });
- const res = await api().post(`/api/auth`, body);
+  const body = JSON.stringify({ email, password });
+  console.log("Login body data: ", body);
+  const res = await api().post(`/api/auth`, body, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
- return res.data
+  return res.data;
 };
 
-export {
- login
-};
+export { login };
