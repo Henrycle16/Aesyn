ba3b4d9b9b4f2f2241d34bae7dab874a02332ff9
Calvin Nguyen <cnguyenr42@gmail.com>
2024-07-27 16:25:44 -0700

    Add settings redirect path to default /settings/account url

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['images.pexels.com', 'www.youtube.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/settings',
+        destination: '/settings/account',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
