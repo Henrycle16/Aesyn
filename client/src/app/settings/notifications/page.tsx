88db4078cbecc5d4f353ab394d45b130558108dd
Justin Talictic <jaytalictic@email.com>
2024-07-27 11:44:09 -0500

    fixed styling

--- client/src/app/settings/notifications/page.tsx
+++ client/src/app/settings/notifications/page.tsx
@@ -5,13 +5,13 @@ import Notifications from "@/components/settings/Notifications";
 
 export default function AccountPage() {
   return (
-    <div className="grid grid-cols-8 my-10 flex flex-col gap-10 w-[77.5rem]">
+    <div className="grid grid-cols-3 my-10 flex flex-col gap-10 w-[77.5rem]">
       {/* Tabs */}
-      <div className="col-start-2 col-span-2">
+      <div className="col-span-1">
         <SettingTabs value={2}></SettingTabs>
       </div>
       {/* Settings Content */}
-      <div className="col-span-4"> 
+      <div className="col-span-2"> 
         <Notifications></Notifications>
       </div>
     </div>
