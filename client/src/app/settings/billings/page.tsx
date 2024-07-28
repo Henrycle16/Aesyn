328620f6615b1489e98d4a70c47904aedd62668f
Calvin Nguyen <cnguyenr42@gmail.com>
2024-07-28 13:57:21 -0700

    Setup Billing and Payments settings skeleton

--- client/src/app/settings/billings/page.tsx
+++ client/src/app/settings/billings/page.tsx
@@ -1,7 +1,10 @@
 export default function BillingPage() {
   return (
-    <div className="">
-      Billings
-    </div>
+    <>
+      <h1 className="heading1 ts5-text">Billing & Payments </h1>
+      <section className="border border-gray-300 rounded-badge p-10 h-[32.063rem]">
+        Lorem Ipsum
+      </section>
+    </>
   );
 }
