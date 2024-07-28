a83bb6ce43b5c3da937fe12d5d3fd7571a223dec
Calvin Nguyen <cnguyenr42@gmail.com>
2024-07-28 14:06:05 -0700

    Setup Notifications setttings skeleton

--- client/src/app/settings/billings/page.tsx
+++ client/src/app/settings/billings/page.tsx
@@ -1,7 +1,7 @@
 export default function BillingPage() {
   return (
     <>
-      <h1 className="heading1 ts5-text">Billing & Payments </h1>
+      <h1 className="heading1 ts5-text">Billing & Payments</h1>
       <section className="border border-gray-300 rounded-badge p-10 h-[32.063rem]">
         Lorem Ipsum
       </section>
--- client/src/app/settings/notifications/page.tsx
+++ client/src/app/settings/notifications/page.tsx
@@ -1,7 +1,10 @@
 export default function NotificationPage() {
   return (
-    <div className="">
-      Notifications
-    </div>
+    <>
+      <h1 className="heading1 ts5-text">Notification Settings</h1>
+      <section className="border border-gray-300 rounded-badge p-10 h-[31.875rem]">
+        Email Notifications
+      </section>
+    </>
   );
 }
