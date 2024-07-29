384f62bd6fae1a0d86fc98f95e6b7f820206fc27
Henry Nguyen <nguyenhenry682@gmail.com>
2024-07-29 09:47:39 -0500

    updated portfolio schemas

--- server/src/models/Creator.ts
+++ server/src/models/Creator.ts
@@ -1,71 +1,123 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const CreatorSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  userName: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  location: {
+    city: {
+      type: String,
     },
-    userName: {
-        type: String
+    state: {
+      type: String,
     },
-    gender: {
-        type: String,
+    country: {
+      type: String,
     },
-    location: {
-        city: {
-            type: String
+  },
+  preferences: {
+    type: [String],
+  },
+  interests: {
+    type: [String],
+  },
+  bio: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  portfolio: {
+    personalContent: [
+      {
+        mediaType: {
+          type: String,
+          required: true,
         },
-        state: {
-            type: String
+        socialMedia: {
+          type: String,
         },
-        country: {
-            type: String
-        }
-    },
-    preferences: {
-        type: [String]
-    },
-    interests: {
-        type: [String]
-    },
-    bio: {
-        type: String
-    },
-    avatar: {
-        type: String,
-    },
-    personalContent: [{
-        type: String // URL or file path to the personal content (picture or video)
-    }],
-    campaignContent: [{
-        type: String // URL or file path to the campaign content (picture or video)
-    }],
-    packages: [
-        {
-            socialMedia: {
-                type: String,
-                // enum: ['Instagram', 'Facebook', 'Twitter/X', 'TikTok', 'YouTube', 'Snapchat']
-            },
-            type: {
-                type: String
-            },
-            description: {
-                type: String
-            },
-            price: {
-                type: Number
-            },
-            quantity: {
-                type: Number
-            }
-        }
+        uri: {
+          type: String,
+          required: true,
+        },
+        // name: {
+        //     type: String,
+        // },
+        campaignTitle: {
+          type: String,
+        },
+        description: {
+          type: String,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
     ],
-    date: {
-        type: Date,
-        default: Date.now,
-    }
+    campaignContent: [
+      {
+        mediaType: {
+          type: String,
+          required: true,
+        },
+        socialMedia: {
+          type: String,
+        },
+        uri: {
+          type: String,
+          required: true,
+        },
+        // name: {
+        //     type: String,
+        // },
+        campaignTitle: {
+          type: String,
+        },
+        description: {
+          type: String,
+        },
+        date: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  packages: [
+    {
+      socialMedia: {
+        type: String,
+        // enum: ['Instagram', 'Facebook', 'Twitter/X', 'TikTok', 'YouTube', 'Snapchat']
+      },
+      type: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+      price: {
+        type: Number,
+      },
+      quantity: {
+        type: Number,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Creator = mongoose.model('Creator', CreatorSchema);
+const Creator = mongoose.model("Creator", CreatorSchema);
 
 export default Creator;
