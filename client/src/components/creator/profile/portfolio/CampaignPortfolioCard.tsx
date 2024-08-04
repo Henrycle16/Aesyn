97ec3f2af13466cda68b6c98e7eac72b46f59194
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-04 13:53:32 -0500

    completed overlay effect on images and video for campaign

--- client/src/components/creator/profile/portfolio/CampaignPortfolioCard.tsx
+++ client/src/components/creator/profile/portfolio/CampaignPortfolioCard.tsx
@@ -6,7 +6,10 @@ import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutl
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import Image from "next/legacy/image";
 
-import { creatorContentInfo, editContent } from "@/redux/slices/creatorPortfolio-slice";
+import {
+  creatorContentInfo,
+  editContent,
+} from "@/redux/slices/creatorPortfolio-slice";
 import { AppDispatch } from "@/redux/store";
 import { useDispatch } from "react-redux";
 
@@ -28,56 +31,76 @@ const CampaignPortfolioCard = (props: Props) => {
   const [isReverse, setIsReverse] = useState(false);
   const [isClient, setIsClient] = useState(false);
 
-  const imageStyle = isHovered ? { filter: "grayscale(1)" } : {};
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
+  const overlayStyle = {
+    content: '""',
+    position: "absolute" as "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(128, 128, 128, 0.5)",
+    zIndex: 1,
+    display: isHovered ? "block" : "none",
+  };
+
   return (
     <div className="relative">
       {props.uri && isClient ? (
         props.mediaType === "image" ? (
-          <Image
-            src={props.thumbnailUri}
-            alt="personal content"
-            width={500}
-            height={400}
-            objectFit="cover"
-            className="rounded"
-            style={{ ...imageStyle, cursor: 'pointer' }}
+          <div
+            className="relative w-[17.35rem] h-[13.88rem] rounded overflow-hidden"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             onClick={() => {
               dispatch(creatorContentInfo({ currentContent: props }));
               (
                 document.getElementById(
-                  `view_campaign_modal`
+                  `view_content_modal`
                 ) as HTMLDialogElement
               ).showModal();
             }}
-          />
+          >
+            <Image
+              src={props.thumbnailUri}
+              alt="personal content"
+              width={500}
+              height={400}
+              objectFit="cover"
+              className="rounded"
+              style={{ cursor: "pointer" }}
+            />
+            <div style={overlayStyle}></div>
+          </div>
         ) : (
-          <div className="w-[17.35rem] h-[13.88rem] max-w-full max-h-full rounded overflow-hidden">
-            <ReactPlayer
-              url={props.uri}
-              light={true}
-              width="100%"
-              height="100%"
-              playIcon={<></>}
-              style={imageStyle}
+          <div className="relative w-[17.35rem] h-[13.88rem] max-w-full max-h-full rounded overflow-hidden">
+            <div
+              className="relative w-full h-full"
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
               onClick={() => {
                 dispatch(creatorContentInfo({ currentContent: props }));
                 (
                   document.getElementById(
-                    `view_campaign_modal`
+                    `view_content_modal`
                   ) as HTMLDialogElement
                 ).showModal();
               }}
-            />
+            >
+              <ReactPlayer
+                url={props.uri}
+                light={true}
+                width="100%"
+                height="100%"
+                playing={false}
+              />
+              <div style={overlayStyle}></div>
+            </div>
           </div>
         )
       ) : (
@@ -87,7 +110,7 @@ const CampaignPortfolioCard = (props: Props) => {
         <>
           <ModeEditOutlineOutlinedIcon
             sx={{ fontSize: 25 }}
-            className={`border-2 rounded-full p-[.12rem] absolute top-2 right-2 cursor-pointer ${
+            className={`z-10 border-2 rounded-full p-[.12rem] absolute top-2 right-2 cursor-pointer ${
               isReverse
                 ? "text-white border-[#3798E3] bg-[#3798E3]"
                 : "text-[#3798E3] border-[#D7D7D7] bg-white"
@@ -112,16 +135,14 @@ const CampaignPortfolioCard = (props: Props) => {
           <DeleteOutlineIcon
             sx={{ color: "#B21717", fontSize: 25 }}
             onClick={() => {
-              dispatch(
-                creatorContentInfo({ currentContent: props })
-              );
+              dispatch(creatorContentInfo({ currentContent: props }));
               (
                 document.getElementById(
                   "delete_content_modal"
                 ) as HTMLDialogElement
               ).showModal();
             }}
-            className="p-[.12rem] absolute bottom-4 left-2 cursor-pointer"
+            className="z-10 p-[.12rem] absolute bottom-4 left-2 cursor-pointer"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           />
