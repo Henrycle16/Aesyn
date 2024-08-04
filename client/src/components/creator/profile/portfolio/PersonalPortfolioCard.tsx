5e580126f2e04a716608b53155e86315e72fe1fd
Henry Nguyen <nguyenhenry682@gmail.com>
2024-08-04 13:50:21 -0500

    completed overlay effect on images and video for personal

--- client/src/components/creator/profile/portfolio/PersonalPortfolioCard.tsx
+++ client/src/components/creator/profile/portfolio/PersonalPortfolioCard.tsx
@@ -28,25 +28,30 @@ const PersonalPortfolioCard = (props: Props) => {
   const [isReverse, setIsReverse] = useState(false);
   const [isClient, setIsClient] = useState(false);
 
-  const imageStyle = isHovered ? { filter: "grayscale(0.65)" } : {};
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
+  const overlayStyle = {
+    content: '""',
+    position: "absolute" as "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "rgba(128, 128, 128, 0.5)",
+    zIndex: 1,
+    display: isHovered ? "block" : "none",
+  };
+
   return (
     <div className="relative">
       {props.uri && isClient ? (
         props.mediaType === "image" ? (
-          <Image
-            src={props.thumbnailUri}
-            alt="personal content"
-            width={500}
-            height={400}
-            objectFit="cover"
-            className="rounded"
-            style={{ ...imageStyle, cursor: "pointer" }}
+          <div
+            className="relative w-[17.35rem] h-[13.88rem] rounded overflow-hidden"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
             onClick={() => {
@@ -57,16 +62,22 @@ const PersonalPortfolioCard = (props: Props) => {
                 ) as HTMLDialogElement
               ).showModal();
             }}
-          />
+          >
+            <Image
+              src={props.thumbnailUri}
+              alt="personal content"
+              width={500}
+              height={400}
+              objectFit="cover"
+              className="rounded"
+              style={{ cursor: "pointer" }}
+            />
+            <div style={overlayStyle}></div>
+          </div>
         ) : (
-          <div className="w-[17.35rem] h-[13.88rem] max-w-full max-h-full rounded overflow-hidden">
-            <ReactPlayer
-              url={props.uri}
-              light={true}
-              width="100%"
-              height="100%"
-              playIcon={<></>}
-              style={imageStyle}
+          <div className="relative w-[17.35rem] h-[13.88rem] max-w-full max-h-full rounded overflow-hidden">
+            <div
+              className="relative w-full h-full"
               onMouseEnter={() => setIsHovered(true)}
               onMouseLeave={() => setIsHovered(false)}
               onClick={() => {
@@ -77,7 +88,16 @@ const PersonalPortfolioCard = (props: Props) => {
                   ) as HTMLDialogElement
                 ).showModal();
               }}
-            />
+            >
+              <ReactPlayer
+                url={props.uri}
+                light={true}
+                width="100%"
+                height="100%"
+                playing={false}
+              />
+              <div style={overlayStyle}></div>
+            </div>
           </div>
         )
       ) : (
@@ -87,7 +107,7 @@ const PersonalPortfolioCard = (props: Props) => {
         <>
           <ModeEditOutlineOutlinedIcon
             sx={{ fontSize: 25 }}
-            className={`border-2 rounded-full p-[.12rem] absolute top-2 right-2 cursor-pointer ${
+            className={`z-10 border-2 rounded-full p-[.12rem] absolute top-2 right-2 cursor-pointer ${
               isReverse
                 ? "text-white border-[#3798E3] bg-[#3798E3]"
                 : "text-[#3798E3] border-[#D7D7D7] bg-white"
@@ -119,7 +139,7 @@ const PersonalPortfolioCard = (props: Props) => {
                 ) as HTMLDialogElement
               ).showModal();
             }}
-            className="p-[.12rem] absolute bottom-4 left-2 cursor-pointer"
+            className="z-10 p-[.12rem] absolute bottom-4 left-2 cursor-pointer"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           />
