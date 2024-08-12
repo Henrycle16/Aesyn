a2deec8676f6bbd410ce80c10e28724042c49e3a
Calvin Nguyen <cnguyenr42@gmail.com>
2024-08-12 16:34:41 -0700

    Add page and layout files to analytics folder

---
+++ client/src/app/(creator)/analytics/[username]/page.tsx
@@ -0,0 +1,11 @@
+type Params = {
+  username: string;
+};
+
+export default function Page({ params }: { params: Params }) {
+  return (
+    <div>
+      Analytics Page
+    </div>
+  )
+}
--- client/src/app/(creator)/analytics/layout.tsx
+++ client/src/app/(creator)/analytics/layout.tsx
@@ -2,7 +2,7 @@ import SmallFooter from "@/components/footer/SmallFooter";
 import ProfileHeader from "@/components/header/ProfileHeader";
 
 export default function ProfileLayout({
-  children,
+  children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
