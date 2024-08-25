70f5edf3b668bac00df60608f19b0e96dca1d38f
Henry Le <henrycle16@gmail.com>
2024-08-25 00:10:52 -0500

    Pulling data into overview and adding analytics to top section

--- client/src/app/(creator)/analytics/[username]/page.tsx
+++ client/src/app/(creator)/analytics/[username]/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-
 import { useGetCreatorByUsernameQuery } from "@/services/creatorApi";
 import Instagram from "@/components/svgs/Instagram";
 import Tiktok from "@/components/svgs/Tiktok";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Overview from "../_components/Overview";
 import Demographic from "../_components/Demographic";
 import RecentPost from "../_components/RecentPost";
 import AnalyticsProfileCard from "../_components/AnalyticsProfileCard";
+import { getInstagramDataById } from "@/actions/InstagramApi";
 
 type Params = {
   username: string;
@@ -25,10 +25,22 @@ export default function CreatorAnalytics({ params }: { params: Params }) {
     isLoading,
   } = useGetCreatorByUsernameQuery(params.username);
   const [selectedButton, setSelectedButton] = useState("instagram");
+  const [instagramData, setInstagramData] = useState(null);
 
   const initials = profileData
     ? getInitials(profileData.user.firstName, profileData.user.lastName)
     : "";
+
+  useEffect(() => {
+    const fetchInstagramData = async () => {
+      if (profileData) {
+        const instagramDataResponse = await getInstagramDataById(profileData._id);
+        setInstagramData(instagramDataResponse.data);
+      }
+    };
+
+    fetchInstagramData();
+  }, [profileData]);
 
   const SocialTiles = ({
     icon,
@@ -56,7 +68,11 @@ export default function CreatorAnalytics({ params }: { params: Params }) {
     <div className="py-10 w-[77.5rem] mx-auto">
       {/* Top Profile Card */}
       <section className="h-[12.75rem] py-5 px-10 border border-gray-300 rounded-badge flex">
-      <AnalyticsProfileCard profileData={profileData} initials={initials} username={params.username}/>
+        <AnalyticsProfileCard
+          profileData={profileData}
+          initials={initials}
+          username={params.username}
+        />
       </section>
 
       {/* Tab Section */}
@@ -71,7 +87,7 @@ export default function CreatorAnalytics({ params }: { params: Params }) {
       {/* Main Section */}
       <section className="mt-4 mb-10 flex flex-col gap-10 w-[77.5rem]">
         {/* Overview Section */}
-        <Overview />
+        <Overview instagramData={instagramData}/>
         {/* Demographic Section */}
         <Demographic />
         {/* Recent Post */}
--- client/src/app/(creator)/analytics/_components/Overview.tsx
+++ client/src/app/(creator)/analytics/_components/Overview.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 import PeopleAltOutlinedIcon from "@mui/icons-material/PeopleAltOutlined";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
 import CommentOutlinedIcon from "@mui/icons-material/CommentOutlined";
 
-const Overview = () => {
+type ImpressionsAndReach = {
+  value: number;
+  end_time: string;
+};
+
+type InstagramData = {
+  followers_count: number;
+  // This might be wrong with how I am parsing through the object
+  insights: {
+    impressions: ImpressionsAndReach[];
+    reach: ImpressionsAndReach[];
+  };
+};
+
+const Overview = ({
+  instagramData,
+}: {
+  instagramData: InstagramData | null;
+}) => {
+  console.log("Instagram Data:", instagramData); // Log Instagram Data
+
   return (
     <div className="border border-gray-300 rounded-badge min-h-[35.125rem] px-10 pt-10 pb-4">
       <div className="flex justify-between">
@@ -31,28 +51,32 @@ const Overview = () => {
             <PeopleAltOutlinedIcon className="mr-4" />
             Followers
           </div>
-          <div className="text-[#3798E3]">1,234</div>
+          <div className="text-[#3798E3]">
+            {instagramData ? instagramData.followers_count : "Loading..."}
+          </div>
         </div>
+
+        {/* All the following divs are placeholders for the actual data that will be displayed in the future. */}
         <div className="border border-black rounded p-4 heading3 ts7-text flex flex-col items-start gap-2">
           <div className="flex items-center">
             <VisibilityIcon className="mr-4" />
             View
           </div>
-          <div className="text-[#3798E3]">5,678</div>
+          <div className="text-[#3798E3]">111,111</div>
         </div>
         <div className="border border-black rounded p-4 heading3 ts7-text flex flex-col items-start gap-2">
           <div className="flex items-center">
             <ThumbUpAltOutlinedIcon className="mr-4" />
             Likes
           </div>
-          <div className="text-[#3798E3]">91,011</div>
+          <div className="text-[#3798E3]">111,111</div>
         </div>
         <div className="border border-black rounded p-4 heading3 ts7-text flex flex-col items-start gap-2">
           <div className="flex items-center">
             <CommentOutlinedIcon className="mr-4" />
             Comments
           </div>
-          <div className="text-[#3798E3]">1,213</div>
+          <div className="text-[#3798E3]">111,111</div>
         </div>
 
         {/* Graphs */}
