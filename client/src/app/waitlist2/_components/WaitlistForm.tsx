fdbf66099270ecd0f5e1e5460940a3f5cd6ce1ff
Calvin Nguyen <cnguyenr42@gmail.com>
2024-09-03 15:42:08 -0700

    Add conditional styling to join as buttons

--- client/src/app/waitlist2/_components/WaitlistForm.tsx
+++ client/src/app/waitlist2/_components/WaitlistForm.tsx
@@ -1,4 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
 export default function WaitlistForm() {
+  const [isBrand, setIsBrand] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const joinAsBrandBtn = () => {
+    setIsBrand(true);
+    setIsClicked(true);
+  };
+  const joinAsCreatorBtn = () => {
+    setIsBrand(false);
+    setIsClicked(true);
+  };
+
+  const activeButtonStyle = "bg-gradient-to-r from-[#5B58EB] to-[#BB63FF] border-0"
+
   const gradientButtonStyle =
     "bg-gradient-to-r from-[#5B58EB] to-[#BB63FF] rounded-3xl h-[2.8125rem] py-2 px-5 text-sm font-bold hover:from-pink-500 hover:to-orange-500";
 
@@ -23,12 +41,14 @@ export default function WaitlistForm() {
           <div className="mt-1 flex gap-3">
             <button
               type="button"
-              className="w-[6.563rem] h-[2.813rem] border-[3px] rounded-[36px] font-bold text-sm">
+              onClick={joinAsBrandBtn}
+              className={`${isBrand && isClicked ? activeButtonStyle : "border-[3px]"} w-[6.563rem] h-[2.813rem] rounded-[36px] font-bold text-sm`}>
               Brand
             </button>
             <button
               type="button"
-              className="w-[6.563rem] h-[2.813rem] border-[3px] rounded-[36px] font-bold text-sm">
+              onClick={joinAsCreatorBtn}
+              className={`${!isBrand && isClicked ? activeButtonStyle : "border-[3px]"} w-[6.563rem] h-[2.813rem] rounded-[36px] font-bold text-sm`}>
               Creator
             </button>
           </div>
