7a5b52f12f3373af1f4721e7b64fcf4e3edecca1
antho <anthonynuge0509@gmail.com>
2024-09-05 17:34:59 -0500

    Map instagram api response into cleaner format.

--- server/src/lib/instagram/instagramCheck.ts
+++ server/src/lib/instagram/instagramCheck.ts
@@ -1,112 +1,144 @@
-import { getPageId, getBusinessId, getBasicUserInfo, getLongLivedAccessToken, getMonthlyUserImpressionsAndReach, getFollowerDemographics_Gender, getFollowerDemographics_Age, getFollowerDemographics_TopCities } from '../../services/instagramGraphAPI';
+import {
+  getPageId,
+  getBusinessId,
+  getBasicUserInfo,
+  getLongLivedAccessToken,
+  getMonthlyUserImpressionsAndReach,
+  getFollowerDemographics_Gender,
+  getFollowerDemographics_Age,
+  getFollowerDemographics_TopCities,
+  getLastMonthData,
+} from "../../services/instagramGraphAPI";
 //import instagram_data from '../../models/InstagramData';
-import InstagramData from '../../models/InstagramData';
+import InstagramData from "../../models/InstagramData";
 
 interface BasicUserInfo {
-    name: string;
-    userName: string;
-    profilePicURL: string;
-    followers_count: string;
+  name: string;
+  userName: string;
+  profilePicURL: string;
+  followers_count: string;
 }
 
 const instagramUserCheck = async (accessToken: string, creatorId: string) => {
-    try {
+  try {
+    // Get pageID of user
+    const pageID = await getPageId(accessToken);
 
-        // Get pageID of user
-        const pageID = await getPageId(accessToken);
+    // Get business id
+    const businessID = await getBusinessId(pageID, accessToken);
 
-        // Get business id
-        const businessID = await getBusinessId(pageID, accessToken);
+    // Check if user exists with businessID
+    const existingUser = await InstagramData.findOne({
+      businessID: businessID,
+    });
 
-        // Check if user exists with businessID
-        const existingUser = await InstagramData.findOne({ businessID: businessID })
-        
-        if(!existingUser){
+    if (!existingUser) {
+      // Get basic user info
+      const basicUserInfo = (await getBasicUserInfo(
+        businessID,
+        accessToken,
+      )) as BasicUserInfo;
+      const { name, userName, profilePicURL, followers_count } = basicUserInfo;
 
-            // Get basic user info
-            const basicUserInfo = await getBasicUserInfo(businessID, accessToken) as BasicUserInfo;
-            const { name, userName, profilePicURL, followers_count } = basicUserInfo;
+      // const testCreatorID = '667cd4f6436c83a8503fec68';
 
-            // const testCreatorID = '667cd4f6436c83a8503fec68';
+      const longtoken = await getLongLivedAccessToken(accessToken);
 
-            const longtoken = await getLongLivedAccessToken(accessToken);
+      const userPayload = {
+        creatorID: creatorId,
+        pageID: pageID,
+        businessID: businessID,
+        longLivedAccessToken: longtoken,
+        name: name,
+        userName: userName,
+        profilePicURL: profilePicURL,
+        followers_count: followers_count,
+      };
 
-            const userPayload = {
-                creatorID: creatorId,
-                pageID: pageID,
-                businessID: businessID,
-                longLivedAccessToken: longtoken,
-                name: name,
-                userName: userName,
-                profilePicURL: profilePicURL,
-                followers_count: followers_count
-            };
+      console.log("User created successfully");
 
-            console.log('User created successfully')
-
-            return userPayload;
-        }else{
-            await getInsights(businessID);
-        }
-
-    } catch (error) {
-        console.log(error);
-        return error;
+      return userPayload;
+    } else {
+      await getInsights(businessID);
     }
-    
-}
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const instagramInsights = async (businessID: string, accessToken: string) => {
-    try {
+  try {
+    const monthylyImpressionsAndReach = await getMonthlyUserImpressionsAndReach(
+      businessID,
+      accessToken,
+    );
+    const followersGender = await getFollowerDemographics_Gender(
+      businessID,
+      accessToken,
+    );
+    const followersAge = await getFollowerDemographics_Age(
+      businessID,
+      accessToken,
+    );
+    const followersTopCities = await getFollowerDemographics_TopCities(
+      businessID,
+      accessToken,
+    );
 
-        const monthylyImpressionsAndReach = await getMonthlyUserImpressionsAndReach(businessID, accessToken)
-        const followersGender = await getFollowerDemographics_Gender(businessID, accessToken)
-        const followersAge = await getFollowerDemographics_Age(businessID, accessToken)
-        const followersTopCities = await getFollowerDemographics_TopCities(businessID, accessToken)
-        
-        return {monthylyImpressionsAndReach, followersGender, followersAge, followersTopCities};
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
-    
-}
+    const dailyMetrics = await getLastMonthData(businessID, accessToken);
+
+    return {
+      monthylyImpressionsAndReach,
+      followersGender,
+      followersAge,
+      followersTopCities,
+      dailyMetrics,
+    };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 const getInsights = async (businessID: string) => {
-    // Find the user with the given businessId
-    const user = await InstagramData.findOne({ businessID: businessID });
+  // Find the user with the given businessId
+  const user = await InstagramData.findOne({ businessID: businessID });
 
-    if (!user) {
-        throw new Error('User not found');
-    }
+  if (!user) {
+    throw new Error("User not found");
+  }
 
-    // Get the accessToken from the user
-    const { longLivedAccessToken: accessToken } = user;
+  // Get the accessToken from the user
+  const { longLivedAccessToken: accessToken } = user;
 
-    // Update basic user info
-    const basicUserInfo = await getBasicUserInfo(businessID, accessToken) as BasicUserInfo;
-    const { name, userName, profilePicURL, followers_count } = basicUserInfo;
+  // Update basic user info
+  const basicUserInfo = (await getBasicUserInfo(
+    businessID,
+    accessToken,
+  )) as BasicUserInfo;
+  const { name, userName, profilePicURL, followers_count } = basicUserInfo;
 
-    // Update the user with the basicUserInfo
-    user.name = name;
-    user.userName = userName;
-    user.profilePicURL = profilePicURL;
-    user.followers_count = followers_count;
+  // Update the user with the basicUserInfo
+  user.name = name;
+  user.userName = userName;
+  user.profilePicURL = profilePicURL;
+  user.followers_count = followers_count;
 
-    const userInsights = await instagramInsights(businessID, accessToken);
+  const userInsights = await instagramInsights(businessID, accessToken);
 
-    if (userInsights) {
-        // Update the user with the insights
-        user.insights = userInsights;
-        const updatedUser = await user.save();
+  if (userInsights) {
+    // Update the user with the insights
+    user.insights = userInsights;
+    const updatedUser = await user.save();
 
-        console.log('User updated with insights and basic user info');
+    console.log("User updated with insights and basic user info");
 
-        return updatedUser;
-    } else {
-        throw new Error('No insights available');
-    }
-}
-
+    return updatedUser;
+  } else {
+    throw new Error("No insights available");
+  }
+};
 
 export { instagramUserCheck, getInsights };
+
--- server/src/services/instagramGraphAPI.ts
+++ server/src/services/instagramGraphAPI.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const getPageId = async (ACCESS_TOKEN: string) => {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/me/accounts?access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/me/accounts?access_token=${ACCESS_TOKEN}`,
     );
 
     const pageId = response.data.data[0].id;
@@ -18,7 +18,7 @@ const getPageId = async (ACCESS_TOKEN: string) => {
 const getBusinessId = async (PAGE_ID: string, ACCESS_TOKEN: string) => {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${PAGE_ID}?fields=instagram_business_account&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${PAGE_ID}?fields=instagram_business_account&access_token=${ACCESS_TOKEN}`,
     );
 
     const businessId = response.data.instagram_business_account.id;
@@ -33,7 +33,7 @@ const getBusinessId = async (PAGE_ID: string, ACCESS_TOKEN: string) => {
 const getBasicUserInfo = async (BUSINESS_ID: string, ACCESS_TOKEN: string) => {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}?fields=name,username,profile_picture_url,followers_count&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}?fields=name,username,profile_picture_url,followers_count&access_token=${ACCESS_TOKEN}`,
     );
 
     const name = response.data.name;
@@ -51,7 +51,7 @@ const getBasicUserInfo = async (BUSINESS_ID: string, ACCESS_TOKEN: string) => {
 const getLongLivedAccessToken = async (ACCESS_TOKEN: string) => {
   try {
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&fb_exchange_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/oauth/access_token?grant_type=fb_exchange_token&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&fb_exchange_token=${ACCESS_TOKEN}`,
     );
     const longLivedAccessToken = response.data.access_token;
 
@@ -64,12 +64,12 @@ const getLongLivedAccessToken = async (ACCESS_TOKEN: string) => {
 
 const getMonthlyUserImpressionsAndReach = async (
   BUSINESS_ID: string,
-  ACCESS_TOKEN: string
+  ACCESS_TOKEN: string,
 ) => {
   try {
     // Get monhtly impressions & reach
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=impressions,reach&period=days_28&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=impressions,reach&period=days_28&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
     );
 
     const impressions = response.data.data[0].values;
@@ -84,15 +84,16 @@ const getMonthlyUserImpressionsAndReach = async (
 
 const getFollowerDemographics_Gender = async (
   BUSINESS_ID: string,
-  ACCESS_TOKEN: string
+  ACCESS_TOKEN: string,
 ) => {
   try {
     // Get last 90 days follower demographics - gender
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=gender&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=gender&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
     );
 
-    const followersGender = response.data.data[0].total_value.breakdowns[0].results;
+    const followersGender =
+      response.data.data[0].total_value.breakdowns[0].results;
 
     return followersGender;
   } catch (error) {
@@ -103,15 +104,16 @@ const getFollowerDemographics_Gender = async (
 
 const getFollowerDemographics_Age = async (
   BUSINESS_ID: string,
-  ACCESS_TOKEN: string
+  ACCESS_TOKEN: string,
 ) => {
   try {
     // Get last 90 days follower demographics - age
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=age&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=age&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
     );
 
-    const followersAge = response.data.data[0].total_value.breakdowns[0].results;
+    const followersAge =
+      response.data.data[0].total_value.breakdowns[0].results;
 
     return followersAge;
   } catch (error) {
@@ -122,18 +124,21 @@ const getFollowerDemographics_Age = async (
 
 const getFollowerDemographics_TopCities = async (
   BUSINESS_ID: string,
-  ACCESS_TOKEN: string
+  ACCESS_TOKEN: string,
 ) => {
   try {
     // Get last 90 days follower demographics - city
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=city&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=city&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
     );
 
-    let followersTopCities = response.data.data[0].total_value.breakdowns[0].results;
+    let followersTopCities =
+      response.data.data[0].total_value.breakdowns[0].results;
 
     // Sort cities by value in descending order and get top 5
-    followersTopCities = followersTopCities.sort((a, b) => b.value - a.value).slice(0, 5);
+    followersTopCities = followersTopCities
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 5);
 
     return followersTopCities;
   } catch (error) {
@@ -145,12 +150,12 @@ const getFollowerDemographics_TopCities = async (
 const getOnlineFollowers = async (
   BUSINESS_ID: string,
   ACCESS_TOKEN: string,
-  TIMESTAMP: string
+  TIMESTAMP: string,
 ) => {
   try {
     // Get online followers per hour per day
     const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=online_followers&period=lifetime&since=${TIMESTAMP}&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=online_followers&period=lifetime&since=${TIMESTAMP}&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
     );
 
     const onlineFollowers = response.data.data[0].values;
@@ -163,64 +168,151 @@ const getOnlineFollowers = async (
 };
 
 const getReachedDemographic_Gender = async (
-    BUSINESS_ID: string,
-    ACCESS_TOKEN: string,
-  ) => {
-    try {
-      // Get reached audience demographics last 90 days - gender
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=gender&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
-      );
-  
-      const reachedGender = response.data.data[0].total_value.breakdowns[0].results;
-  
-      return reachedGender;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  };
+  BUSINESS_ID: string,
+  ACCESS_TOKEN: string,
+) => {
+  try {
+    // Get reached audience demographics last 90 days - gender
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=gender&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
+    );
 
-  const getReachedDemographic_Age = async (
-    BUSINESS_ID: string,
-    ACCESS_TOKEN: string,
-  ) => {
-    try {
-      // Get reached audience demographics last 90 days - age
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=age&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
-      );
-  
-      const reachedAge = response.data.data[0].total_value.breakdowns[0].results;
-  
-      return reachedAge;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  };
+    const reachedGender =
+      response.data.data[0].total_value.breakdowns[0].results;
 
-  const getReachedDemographic_TopCities = async (
-    BUSINESS_ID: string,
-    ACCESS_TOKEN: string,
-  ) => {
-    try {
-      // Get reached audience demographics last 90 days - age
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=city&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`
-      );
-  
-      let reachedTopCities = response.data.data[0].total_value.breakdowns[0].results;
+    return reachedGender;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const getReachedDemographic_Age = async (
+  BUSINESS_ID: string,
+  ACCESS_TOKEN: string,
+) => {
+  try {
+    // Get reached audience demographics last 90 days - age
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=age&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
+    );
+
+    const reachedAge = response.data.data[0].total_value.breakdowns[0].results;
+
+    return reachedAge;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+const getReachedDemographic_TopCities = async (
+  BUSINESS_ID: string,
+  ACCESS_TOKEN: string,
+) => {
+  try {
+    // Get reached audience demographics last 90 days - age
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=reached_audience_demographics&period=lifetime&timeframe=last_90_days&breakdown=city&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
+    );
+
+    let reachedTopCities =
+      response.data.data[0].total_value.breakdowns[0].results;
 
     // Sort cities by value in descending order and get top 5
-    reachedTopCities = reachedTopCities.sort((a, b) => b.value - a.value).slice(0, 5);
+    reachedTopCities = reachedTopCities
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 5);
 
     return reachedTopCities;
-    } catch (error) {
-      console.log(error);
-      return error;
-    }
-  };
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+// Ran when data is first grabbed. Retrieves the daily data for a month ago
+const getLastMonthData = async (BUSINESS_ID: string, ACCESS_TOKEN: string) => {
+  try {
+    const currentDate = new Date();
+
+    // Get yesterday date value since complete day
+    const yesterday = new Date(currentDate);
+    yesterday.setDate(currentDate.getDate() - 1);
+
+    // Get date one month ago
+    const lastMonth = new Date();
+    lastMonth.setMonth(lastMonth.getMonth() - 1);
+
+    // Convert to unix time frame
+    const yesterdayUnix = Math.floor(yesterday.getTime() / 1000);
+    const lastMonthUnix = Math.floor(lastMonth.getTime() / 1000);
+
+    console.log("Retrieving data between", lastMonth, yesterday);
+    console.log("Unix Time Conversions ", lastMonth, yesterday);
+
+    // Get monhtly impressions & reach
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=impressions,reach&period=day&since=${lastMonthUnix}&until=${yesterdayUnix}&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
+    );
+
+    // Extract only the response data
+    const impressionResponse = response.data.data[0].values;
+    const reachResponse = response.data.data[1].values;
+
+    // Combine impressions and reach into a consolidated object object {date, impression, reach}
+    const dailyMetrics = impressionResponse.map((impression, index) => {
+      const reach = reachResponse[index];
+      return {
+        date: impression.end_time,
+        impression: impression.value,
+        reach: reach.value,
+      };
+    });
+
+    return dailyMetrics;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
+
+// Return a object with the followers account for genders male, female, and other
+const getFollowerDemographics_GenderFormatted = async (
+  BUSINESS_ID: string,
+  ACCESS_TOKEN: string,
+) => {
+  try {
+    // Get last 90 days follower demographics - gender
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_HEADER}/${BUSINESS_ID}/insights?metric=follower_demographics&period=lifetime&timeframe=last_90_days&breakdown=gender&metric_type=total_value&client_id=${process.env.NEXT_PUBLIC_APP_ID}&client_secret=${process.env.NEXT_PUBLIC_APP_SECRET}&access_token=${ACCESS_TOKEN}`,
+    );
+
+    const followersGenderResponse =
+      response.data.data[0].total_value.breakdowns[0].results;
+
+    // Map into array of { gender : followersCount } lengh for each gender
+    const followerCountByGender = followersGenderResponse.reduce(
+      (acc, item) => {
+        const gender = item.dimension_values[0];
+
+        if (gender === "F") {
+          acc["female"] = item.value;
+        } else if (gender === "M") {
+          acc["male"] = item.value;
+        } else {
+          acc["other"] = item.value;
+        }
+        return acc;
+      },
+      {},
+    );
+    return followerCountByGender;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+};
 
 export {
   getPageId,
@@ -235,4 +327,6 @@ export {
   getReachedDemographic_Gender,
   getReachedDemographic_Age,
   getReachedDemographic_TopCities,
+  getLastMonthData,
+  getFollowerDemographics_GenderFormatted,
 };
