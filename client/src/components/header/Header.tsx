0943d4fd662060d4e42e9425c88c48b1a35e1f12
Calvin Nguyen <cnguyenr42@gmail.com>
2024-09-06 13:36:35 -0700

    Add styling to header for waitlist page

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+  
   return (
     <header className="flex flex-wrap px-16 pt-5 flex-col md:flex-row items-center">
-      <Link href={"/"} className="text-2xl text-gray-900 font-semibold">
+      <Link href={"/"} className={`${pathname ==='/waitlist' ? 'text-white' : 'text-gray-900'} text-2xl font-semibold`}>
         ShareFluence
       </Link>
     </header>
