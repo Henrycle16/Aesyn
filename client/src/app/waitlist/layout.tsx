9ebe3ce016f099a80907a5ae880bf8790dcbe297
Calvin Nguyen <77700030+calvinguyen@users.noreply.github.com>
2024-09-07 11:40:25 -0700

    UI/waitlist styling (#105)
    * Fix gradient background
    
    * Add cutom arrow icon to select field

--- client/src/app/waitlist/layout.tsx
+++ client/src/app/waitlist/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#36035F] from-38% via-[#240B4D] via-50% to-[#000000] to-76%">
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#36035F] from-0% via-[#240B4D] via-19% to-[#000000] to-65%">
       <Header />
       <main className="container flex-1 flex items-center justify-center mx-auto my-10">
         {children}
