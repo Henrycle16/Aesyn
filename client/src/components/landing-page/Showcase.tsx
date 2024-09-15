081f42690e8a3d73863536732997933a2f749865
Henry Nguyen <nguyenhenry682@gmail.com>
2024-09-15 15:11:43 -0500

    added highlight styling

--- client/src/components/landing-page/Showcase.tsx
+++ client/src/components/landing-page/Showcase.tsx
@@ -16,10 +16,12 @@ const Showcase = ({feature}: ShowcaseProps) => {
   // Feature 1: < 11
   // Feature 2: 11-14
   // Feature 3: > 14
-  // Need to adjust scrolling amount (can be done by changing height of parent container)
 
   const [imgSrc, setImgSrc] = useState<string>("/showcase_analytics.png");
   const [section, setSection] = useState<string>("feature-1");
+
+  const sectionHighlight = "bg-gradient-to-br from-[#FDE9FF] to-[#F3EAFF] from-0% to-100%"
+  const sectionStyle = "border-[1px] shadow-lg px-20 py-4 rounded-xl"
 
   useEffect(() => {
     if (feature < 11) {
@@ -51,17 +53,17 @@ const Showcase = ({feature}: ShowcaseProps) => {
 
         {/* Left Side: Description */}
         <div className="grid grid-cols-[45%_55%] gap-10 items-center justify-center w-[62rem]">
-          <div className="border-[1px] border-[#F153FF] min-h-[250px] bg-gradient-to-br from-[#ffffff4d] to-[#ffffff26] from-0% to-100% px-10 py-10 flex flex-col gap-y-5 rounded-xl">
+          <div className="shadow-lg border-[1px] min-h-[250px] bg-gradient-to-br from-[#ffffffce] to-[#ffffff26] from-0% to-100% px-10 py-10 flex flex-col gap-y-5 rounded-xl">
             <h3 className="font-semibold text-2xl">Lorem ipsum dolor sit amet, consectetu</h3>
             <p className="text-[#4A4A4A] text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun</p>
             <div className="gap-y-5 flex flex-col font-semibold my-10">
-              <div className={`${section === "feature-1" ? "text-blue-500" : ""} border-[1px] border-[#F153FF] px-20 py-4 rounded-xl`}>
+              <div className={`${section === "feature-1" ? `${sectionHighlight}` : ""} ${sectionStyle}`}>
                 Section 1
               </div>
-              <div className={`${section === "feature-2" ? "text-blue-500" : ""} border-[1px] border-[#F153FF] px-20 py-4 rounded-xl`}>
+              <div className={`${section === "feature-2" ? `${sectionHighlight}` : ""} ${sectionStyle}`}>
                 Section 2
               </div>
-              <div className={`${section === "feature-3" ? "text-blue-500" : ""} border-[1px] border-[#F153FF] px-20 py-4 rounded-xl`}>
+              <div className={`${section === "feature-3" ? `${sectionHighlight}` : ""} ${sectionStyle}`}>
                 Section 3
               </div>
             </div>
