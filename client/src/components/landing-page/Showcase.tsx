9c7b97497b3e874d73f057666848b2b099962600
Henry <91310711+SX129@users.noreply.github.com>
2024-09-15 16:48:12 -0500

    merge feature/landing page/showcase (#112)
    * initial commit
    
    * fixed border
    
    * fixed height
    
    * initial commit
    
    * second commit
    
    * 3rd commit
    
    * added switch case
    
    * this is good starting commit
    
    * reset
    
    * good reset point
    
    * THE BEST STARTING SPOT
    
    * set props
    
    * BREAKTHROUGH
    
    * bless me
    
    * added highlight styling
    
    * added gradient border on hightlight
    
    * finalized gradient border styling

--- client/src/components/landing-page/Showcase.tsx
+++ client/src/components/landing-page/Showcase.tsx
@@ -1,11 +1,44 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import "@/styles/gradientBorder.css";
+interface ShowcaseProps {
+  feature: number;
+  scrollDirection: string;
+}
 
-const Showcase = () => {
+const Showcase = ({ feature }: ShowcaseProps) => {
+  // Scrolling up goes from feature-17 to feature-5
+  // Scrolling down goes from feature-8 to feature-21
+
+  // Feature 1: < 11
+  // Feature 2: 11-14
+  // Feature 3: > 14
+
+  const [imgSrc, setImgSrc] = useState<string>("/showcase_analytics.png");
+  const [section, setSection] = useState<string>("feature-1");
+
+  const sectionHighlight =
+    "bg-gradient-to-br from-[#FDE9FF] to-[#F3EAFF] from-0% to-100%";
+  const sectionStyle = "border-[1px] shadow-lg px-20 py-4 rounded-xl";
+
+  useEffect(() => {
+    if (feature < 12) {
+      setSection("feature-1");
+      setImgSrc("/showcase_analytics.png");
+    } else if (feature >= 12 && feature <= 15) {
+      setSection("feature-2");
+      setImgSrc("/showcase_analytics_2.png");
+    } else if (feature > 15) {
+      setSection("feature-3");
+      setImgSrc("/showcase_analytics_3.png");
+    }
+  }, [feature]);
+
   return (
     <>
-      <div className="flex flex-col items-center max-w-7xl py-40">
+      <div className="flex flex-col items-center max-w-7xl">
         <h1 className="text-[2.5rem] text-center leading-[3rem] font-semibold">
           Lorem ipsum dolor sit amet, consectetu
         </h1>
@@ -15,31 +48,58 @@ const Showcase = () => {
           tempor
         </p>
 
-        {/* Left Side: Description */}
-        <div className="grid grid-cols-[45%_55%] gap-10 items-center justify-center w-[62rem]">
-          <div className="border-[1px] border-[#F153FF] min-h-[250px] bg-gradient-to-br from-[#ffffff4d] to-[#ffffff26] from-0% to-100% px-10 py-10 flex flex-col gap-y-5 rounded-xl">
-            <h3 className="font-semibold text-2xl">Lorem ipsum dolor sit amet, consectetu</h3>
-            <p className="text-[#4A4A4A] text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun</p>
+        <div className="grid grid-cols-[45%_55%] gap-10 items-center justify-center w-[70rem]">
+          {/* Left Side: Description */}
+          <div className="feature-container-gradient shadow-lg min-h-[250px] bg-gradient-to-br from-[#ffffffb0] to-[#ffffff86] from-0% to-100% px-10 py-10 flex flex-col gap-y-5 rounded-xl">
+            <h3 className="font-semibold text-2xl">
+              Lorem ipsum dolor sit amet, consectetu
+            </h3>
+            <p className="text-[#4A4A4A] text-base">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididun
+            </p>
             <div className="gap-y-5 flex flex-col font-semibold my-10">
-              <div className="border-[1px] border-[#F153FF] px-20 py-4 rounded-xl">
-                Section 1
+              <div
+                className={`${
+                  section === "feature-1" ? `feature-gradient` : ""
+                }`}>
+                <div
+                  className={`${
+                    section === "feature-1" ? `${sectionHighlight}` : ""
+                  } ${sectionStyle}`}>
+                  Section 1 - Lorem ipsum dolor sit amet
+                </div>
               </div>
-              <div className="border-[1px] border-[#F153FF] px-20 py-4 rounded-xl">
-                Section 2
+
+              <div
+                className={`${
+                  section === "feature-2" ? `feature-gradient` : ""
+                }`}>
+                <div
+                  className={`${
+                    section === "feature-2" ? `${sectionHighlight}` : ""
+                  } ${sectionStyle}`}>
+                  Section 2 - Lorem ipsum dolor sit amet
+                </div>
               </div>
-              <div className="border-[1px] border-[#F153FF] px-20 py-4 rounded-xl">
-                Section 3
+
+              <div
+                className={`${
+                  section === "feature-3" ? `feature-gradient` : ""
+                }`}>
+                <div
+                  className={`${
+                    section === "feature-3" ? `${sectionHighlight}` : ""
+                  } ${sectionStyle}`}>
+                  Section 3 - Lorem ipsum dolor sit amet
+                </div>
               </div>
             </div>
           </div>
 
           {/* Right Side: Image */}
           <div className="">
-            <Image
-              src="/showcase_analytics.png"
-              alt="Showcase"
-              width={600}
-              height={600} />
+            <Image src={imgSrc} alt="Showcase" width={600} height={600} />
           </div>
         </div>
       </div>
