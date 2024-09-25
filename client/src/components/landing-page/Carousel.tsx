7bca9283ecdcbe3592e9c84dac4a0bc522b76d68
Calvin Nguyen <cnguyenr42@gmail.com>
2024-09-24 18:11:11 -0700

    Setup carousel container

--- client/src/components/landing-page/Carousel.tsx
+++ client/src/components/landing-page/Carousel.tsx
@@ -3,10 +3,8 @@
 const Carousel = () => {
   return (
     <>
-      <div className="flex flex-col items-center max-w-7xl py-40 border-2 border-red-500">
-        <h1 className="text-[2.5rem] text-center leading-[3rem] font-semibold">
-          Lorem ipsum dolor sit amet, consectetu adipiscing
-        </h1>
+      <div className="w-full h-[70vh] border border-red-500">
+        
       </div>
     </>
   );
