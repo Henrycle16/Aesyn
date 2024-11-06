33e81191df347e5edd91dd67545781259293636a
Calvin Nguyen <cnguyenr42@gmail.com>
2024-11-05 17:25:50 -0800

    Change carousel logic to only show certain amt of cards

--- client/src/components/landing-page/Carousel.tsx
+++ client/src/components/landing-page/Carousel.tsx
@@ -1,17 +1,3 @@
-/* "use client";
-
-const Carousel = () => {
-  return (
-    <>
-      <div className="w-full h-[70vh] border border-red-500">
-        
-      </div>
-    </>
-  );
-};
-
-export default Carousel; */
-
 "use client";
 
 import gsap from "gsap";
@@ -45,23 +31,22 @@ const Carousel = () => {
     window.addEventListener("resize", setup);
 
     gsap.to(".wheel", {
-      rotate: () => -360,
+      rotate: () => -192,
       ease: "none",
-      duration: cards.length,
       scrollTrigger: {
         start: "top top", 
-        end: "+=1000",
-        scrub: 0.5,
+        end: "+=1800",
+        scrub: 0.3,
         pin: true,
         snap: {
-          snapTo: 1 / cards.length, 
+          snapTo: 1 / 8, 
           duration: { min: 0.2, max: 0.7 }, 
           ease: "power1.inOut", 
           inertia: false,
         },
         invalidateOnRefresh: true,
         trigger: "#carousel",
-        endTrigger: "landing-footer",
+        endTrigger: "#landing-footer",
       },
     });
   }, []);
@@ -109,71 +94,71 @@ const Carousel = () => {
             </div>
             {/* 6-10 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 1</h3>
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 6</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 2</h3>
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 7</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 3</h3>
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 8</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 4</h3>
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 9</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 5</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 10</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
             {/* 11-15 */}
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 1</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 11</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 2</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 12</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 3</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 13</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 4</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 14</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
               <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
             </div>
-            <div className="wheel__card">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 5</h3>
+            <div className="wheel__card invisible">
+              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 15</h3>
               <p className="mt-4">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit
               </p>
