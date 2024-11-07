11fde7c912127aff3159b50197f31d57667d1805
Henry Le <henrycle16@gmail.com>
2024-11-06 22:18:47 -0600

    removing tailwind lineclamp plugin

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,44 +1,45 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		backgroundImage: {
-  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
-  		},
-  		colors: {
-  			muiblue: '#1976d2'
-  		},
-  		animation: {
-  			'border-beam': 'border-beam calc(var(--duration)*1s) infinite linear',
-			marquee: "marquee var(--duration) linear infinite",
-			"marquee-vertical": "marquee-vertical var(--duration) linear infinite",
-  		},
-  		keyframes: {
-  			'border-beam': {
-  				'100%': {
-  					'offset-distance': '100%'
-  				}
-  			},
-			marquee: {
-			from: { transform: "translateX(0)" },
-			to: { transform: "translateX(calc(-100% - var(--gap)))" },
-			},
-			"marquee-vertical": {
-			from: { transform: "translateY(0)" },
-			to: { transform: "translateY(calc(-100% - var(--gap)))" },
-			},
-  		}
-  	}
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        muiblue: "#1976d2",
+      },
+      animation: {
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+      },
+      keyframes: {
+        "border-beam": {
+          "100%": {
+            "offset-distance": "100%",
+          },
+        },
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-100% - var(--gap)))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-100% - var(--gap)))" },
+        },
+      },
+    },
   },
-  plugins: [require("daisyui"), require("@tailwindcss/line-clamp")],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: [],
     base: false,
