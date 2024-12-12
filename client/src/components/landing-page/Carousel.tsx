964bfac277dc39f452811a30e93302c7cfb2cde8
Calvin Nguyen <henrycle16@gmail.com>
2024-12-11 17:54:14 -0800

    Add images to carousel cards

--- client/src/components/landing-page/Carousel.tsx
+++ client/src/components/landing-page/Carousel.tsx
@@ -6,6 +6,10 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
 import stepOne from "../../../public/carousel-1.png";
+import stepTwo from "../../../public/carousel-2.png";
+import stepThree from "../../../public/carousel-3.png";
+import stepFour from "../../../public/carousel-4.png";
+import stepFive from "../../../public/carousel-5.png";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
@@ -58,130 +62,88 @@ const Carousel = () => {
       <div className="w-full h-[41.219rem] relative overflow-hidden">
         <section className="slider-section pt-[13rem] ">
           <div className="wheel">
-            {/* 1-5 */}
             {/* Step 1 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Business Registration</h3>
-              <p className="mt-4">
-                Brand signs up, registering details like industry, target audience, and social media presence.
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto relative">
-                <Image
-                  src={stepOne}
-                  // width={}
-                  // height={}
-                  alt="Step 1"
-                  fill={true}
-                  style={{ objectFit: "cover"}}
-                />
+              <h3 className="font-bold text-xl">Register Your Brand To Collaborate</h3>
+              <div className="relative">
+                <div className="h-[10.563rem] w-full mt-auto relative top-[2rem]">
+                  <Image
+                    src={stepOne}
+                    alt="Step 1"
+                  />
+                </div>
               </div>
             </div>
+
             {/* Step 2 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Create Campaign Opportunity</h3>
-              <p className="mt-4">
-                Brand launches a campaign with marketing goals, content needs, and specific criteria.
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
+              <h3 className="font-bold text-xl">Launch Campaign Opportunities</h3>
+              <div className="relative">
+                <div className="h-[10.563rem] w-full mt-auto relative top-[5rem]">
+                  <Image
+                    src={stepTwo}
+                    alt="Step 2"
+                  />
+                </div>
+              </div>
             </div>
+
+            {/* Step 3 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Receive & Review Proposals</h3>
-              <p className="mt-4">
-                Creators submit proposals with content ideas, strategies, and pricing packages.
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
+              <h3 className="font-bold text-xl">Review Tailored Proposals</h3>
+              <div className="relative">
+                <div className="h-[10.563rem] w-full mt-auto relative top-[2rem]">
+                  <Image
+                    src={stepThree}
+                    alt="Step 3"
+                  />
+                </div>
+              </div>
             </div>
+
+            {/* Step 4 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Secure Payment & Content Creation</h3>
-              <p className="mt-4">
-                Platform holds payment securely while the creator develops the content.
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
+              <h3 className="font-bold text-xl">Connect With Top Creators</h3>
+              <div className="relative">
+                <div className="h-[10.563rem] w-full mt-auto relative top-[2rem]">
+                  <Image
+                    src={stepFour}
+                    alt="Step 4"
+                  />
+                </div>
+              </div>
             </div>
+
+            {/* Step 5 */}
             <div className="wheel__card">
-              <h3 className="font-bold text-xl">Publish Content & Make an Impact</h3>
-              <p className="mt-4">
-                Brand approves completed content, which goes live to engage the target audience.
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
+              <h3 className="font-bold text-xl">Publish & Drive Impact</h3>
+              <div className="relative">
+                <div className="h-[10.563rem] w-full mt-auto relative top-[2.8rem]">
+                  <Image
+                    src={stepFive}
+                    alt="Step 5"
+                  />
+                </div>
+              </div>
             </div>
-            {/* 6-10 */}
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 6</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 7</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 8</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 9</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 10</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            {/* 11-15 */}
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 11</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 12</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 13</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 14</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
-            <div className="wheel__card invisible">
-              <h3 className="font-bold text-xl">Lorem ipsum dolor sit ame 15</h3>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit
-              </p>
-              <div className="h-[10.563rem] w-full bg-[#D9D9D9] mt-auto"></div>
-            </div>
+
+            <EmptyCards />
+            
           </div>
         </section>
       </div>
     </>
   );
 };
+
+function EmptyCards() {
+  const cards = []
+
+  for (let i = 0; i < 10; i++) {
+    cards.push(<div key={i} className="wheel__card invisible"></div>);
+  }
+
+  return cards;
+}
 
 export default Carousel;
